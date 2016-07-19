ff6de20eee2d907a2b9e46c65a09e52fe800a514
uniquews <wstjpu@gmail.com>
2016-07-19 00:08:49 -0400

    Initial commit

---
+++ src/main/Search2DMatrix.java
@@ -0,0 +1,87 @@
+package main;
+
+/**
+ * Created by shuaiwang on 7/18/16.
+ */
+public class Search2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0)
+        {
+            return false;
+        }
+
+        if (matrix[0] == null || matrix[0].length == 0)
+        {
+            return false;
+        }
+
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int start = 0, end = row - 1;
+        while (start + 1 < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid][0] == target)
+            {
+                return true;
+            }
+            else if (matrix[mid][0] > target)
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid;
+            }
+        }
+
+        if (matrix[end][0] <= target)
+        {
+            row = end;
+        }
+        else if (matrix[start][0] <= target)
+        {
+            row = start;
+        }
+        else
+        {
+            return false;
+        }
+
+
+        start = 0;
+        end = column - 1;
+        while (start + 1 < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (matrix[row][mid] == target)
+            {
+                return true;
+            }
+            else if (matrix[row][mid] > target)
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid;
+            }
+        }
+
+        if (matrix[row][start] == target)
+        {
+            return true;
+        }
+        else if (matrix[row][end] == target)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ src/test/Search2DMatrixTest.java
@@ -0,0 +1,19 @@
+package test;
+
+
+import main.Search2DMatrix;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by shuaiwang on 7/18/16.
+ */
+public class Search2DMatrixTest {
+    @Test
+    public void test() {
+        Search2DMatrix test = new Search2DMatrix();
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        assertEquals(true, test.searchMatrix(matrix, 7));
+    }
+}
