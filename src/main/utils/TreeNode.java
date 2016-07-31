c54c9c750e8b97cd9243c870ca382aedf455b517
uniquews <wstjpu@gmail.com>
2016-07-31 19:06:24 -0400

    abc

---
+++ src/main/utils/TreeNode.java
@@ -0,0 +1,19 @@
+package main.utils;
+
+/**
+ * Created by shuaiwang on 7/26/16.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(val);
+    }
+}
