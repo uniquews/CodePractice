d5195f1af6b947e02475f7fbbc923d0f361925da
uniquews <wstjpu@gmail.com>
2016-09-14 22:47:39 -0400

    abc

---
+++ src/main/utils/ListNode.java
@@ -0,0 +1,13 @@
+package main.utils;
+
+/**
+ * Created by shuaiwang on 9/14/16.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
