4832a50897ac3cff441ddf12c2c1abd78dd1bc10
uniquews <wstjpu@gmail.com>
2016-09-23 00:13:23 -0400

    abc

---
+++ src/main/LinkedListCycleII.java
@@ -0,0 +1,39 @@
+package main;
+
+import main.utils.ListNode;
+
+/**
+ * Created by shuaiwang on 9/22/16.
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        // write your code here
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
