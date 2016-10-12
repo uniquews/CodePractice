38c3281c6972cca80c3965f2ffb3fcd23d0be832
uniquews <wstjpu@gmail.com>
2016-10-11 23:38:13 -0400

    abc

---
+++ src/main/MergeTwoSortedArrays.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by shuaiwang on 10/11/16.
+ */
+public class MergeTwoSortedArrays {
+    public int[] mergeSortedArray(int[] A, int[] B) {
+        // Write your code here
+    }
+}
