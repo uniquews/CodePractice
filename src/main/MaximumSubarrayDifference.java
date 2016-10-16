f4194185add86f82bd015524c9610f15512116ea
uniquews <wstjpu@gmail.com>
2016-10-15 23:04:19 -0400

    lalala

--- src/main/MaximumSubarrayDifference.java
+++ src/main/MaximumSubarrayDifference.java
@@ -2,6 +2,26 @@ package main;
 
 /**
  * Created by shuaiwang on 10/15/16.
+ *
+ * Given an array with integers.
+ * Find two non-overlapping subarrays A and B, which |SUM(A) - SUM(B)| is the largest.
+ * Return the largest difference.
+ *
+ * leftMax[i] is max subarray sum at index i, where nums[i] is added with numbers with index less than i
+ * rightMax[i] is max subarray sum at index i, where nums[i] is added with numbers with index greater than i
+ * leftMin[i] is min subarray sum at index i, where nums[i] is added with numbers with index less than i
+ * rightMin[i] is min subarray sum at index i, where nums[i] is added with numbers with index greater than i;
+ *
+ * input = [1, 2, -3, 1]
+ *
+ * leftMax = [1, 3, 3, 3]
+ * rightMax = [1, 1, 2, 3]
+ * leftMin = [1, 1, -3, -3]
+ * rightMin = [1, -3, -3, -3]
+ *
+ * |SUM(A) - SUM(B)| could be the max subarray, which summed up at left side minus min subarray, which summed up at
+ * right side, and vice versa.
+ *
  */
 public class MaximumSubarrayDifference {
     public int maxDiffSubArrays(int[] nums) {
