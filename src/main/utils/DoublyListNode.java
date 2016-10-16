b70a45a97fa6049c6d406d485d0b1836bdfa7458
uniquews <wstjpu@gmail.com>
2016-10-15 22:38:44 -0400

    abc

---
+++ src/main/utils/DoublyListNode.java
@@ -0,0 +1,13 @@
+package main.utils;
+
+/**
+ * Created by shuaiwang on 9/28/16.
+ */
+public class DoublyListNode {
+    public int val;
+    public DoublyListNode next, prev;
+    public DoublyListNode(int val) {
+      this.val = val;
+      this.next = this.prev = null;
+    }
+}
