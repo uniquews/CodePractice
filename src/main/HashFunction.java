f7dd96d5781f1f170ffee64e1ffdb580054ba0ed
uniquews <wstjpu@gmail.com>
2016-10-18 21:12:32 -0400

    abc

--- src/main/HashFunction.java
+++ src/main/HashFunction.java
@@ -4,26 +4,26 @@ package main;
  * Created by shuaiwang on 10/17/16.
  */
 public class HashFunction {
-    public int hashCode(char[] key,int HASH_SIZE) {
+
+    // Solution 2
+
+/**
+ *
+ *  f(n) = (a * 33^2 + b * 33 + c) % k
+ *  => ((a * 33^2 + b * 33) % k + c % k) % k
+ *  => ((33 * (a * 33 + b)) % k + c % k) % k
+ *  => (((33 % k) * (a * 33 + b) % k) % k + c % k) % k
+ *  => (((33 % k) * f(n - 1)) % k + c % k) % k
+ *  => (((33 % k) * f(n - 1)) + c ) % k
+ *
+ * */
+    public int hashCode(char[] key,int k) {
         // write your code here
-        long base = 1;
-        int result = 0;
-        for (int i = key.length - 1; i >= 0; i--) {
-            int cur = (int)key[i];
-            result += caculateCurrent(cur, base, HASH_SIZE); // subResult = num * base % HASH_SIZE
-            result = result % HASH_SIZE; // (subResult_1 + subResult_2) % HASH_SIZE
-            base = calculateBase(base, 33, HASH_SIZE); // base1 = 33%HASH base2=33%HASH*33%HASH base3=33%HASH*33%HASH*33%HASH
+        long result = 0;
+        for (int i = 0; i < key.length; i++) {
+//            result = (((33 % k) * result) % k + key[i] % k) % k;
+            result = ((33 % k) * result + key[i]) % k;
         }
-        return result;
-    }
-
-    private long calculateBase(long base, long num, int HASH_SIZE) {
-        long tmp = base * num % HASH_SIZE;
-        return tmp;
-    }
-
-    private int caculateCurrent(long num, long base33, int HASH_SIZE) {
-        long tmp = num * base33 % HASH_SIZE;
-        return (int)tmp;
+        return (int) result;
     }
 }
