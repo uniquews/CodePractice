f84a669f34a3b3fc915dbc66f3b2f9bfea3a5cc5
uniquews <wstjpu@gmail.com>
2016-11-14 00:00:39 -0500

    abc

--- src/main/CloneGraph.java
+++ src/main/CloneGraph.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.UnionDV;
 import main.utils.UndirectedGraphNode;
 
 import java.util.*;
@@ -14,7 +15,7 @@ public class CloneGraph {
             return node;
         }
 
-        ArrayList<UndirectedGraphNode> allNodes = getAllNodesByBFS(node);
+        ArrayList<UndirectedGraphNode> allNodes = getAllNodesByDFS(node);
 
         HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
 
@@ -33,22 +34,68 @@ public class CloneGraph {
         return map.get(node);
     }
 
-    private ArrayList<UndirectedGraphNode> getAllNodesByBFS(UndirectedGraphNode start) {
+    // bfs collects all nodes
+//    private ArrayList<UndirectedGraphNode> getAllNodesByBFS(UndirectedGraphNode start) {
+//        HashSet<UndirectedGraphNode> set = new HashSet<>();
+//        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+//
+//        queue.offer(start);
+//        set.add(start);
+//
+//        while (!queue.isEmpty()) {
+//            UndirectedGraphNode current = queue.poll();
+//            for (UndirectedGraphNode node : current.neighbors) {
+//                if (!set.contains(node)) {
+//                    queue.add(node);
+//                    set.add(node);
+//                }
+//            }
+//        }
+//        return new ArrayList<>(set);
+//    }
+
+    // dfs collects all nodes
+    class StackElement {
+        public UndirectedGraphNode node;
+        public Integer neighborIndex;
+        public StackElement(UndirectedGraphNode n, Integer index) {
+            node = n;
+            neighborIndex = index;
+        }
+    }
+    private ArrayList<UndirectedGraphNode> getAllNodesByDFS(UndirectedGraphNode start) {
+        Stack<StackElement> stack = new Stack<>();
         HashSet<UndirectedGraphNode> set = new HashSet<>();
-        Queue<UndirectedGraphNode> queue = new LinkedList<>();
 
-        queue.offer(start);
+        StackElement startStk = new StackElement(start, -1);
+        stack.add(startStk);
         set.add(start);
+        while (!stack.isEmpty()) {
+            StackElement current = stack.peek();
+            current.neighborIndex++;
+            if (current.neighborIndex == current.node.neighbors.size()) {
+                stack.pop();
+                continue;
+            }
 
-        while (!queue.isEmpty()) {
-            UndirectedGraphNode current = queue.poll();
-            for (UndirectedGraphNode node : current.neighbors) {
-                if (!set.contains(node)) {
-                    queue.add(node);
-                    set.add(node);
-                }
+            UndirectedGraphNode nextNode = current.node.neighbors.get(current.neighborIndex);
+
+            if (set.contains(nextNode)) {
+                continue;
+            } else{
+                StackElement nextStkElem = new StackElement(nextNode, -1);
+                stack.add(nextStkElem);
+                set.add(nextNode);
             }
         }
         return new ArrayList<>(set);
+    }
+
+    public static void main(String[] args) {
+        CloneGraph test = new CloneGraph();
+        UndirectedGraphNode a = new UndirectedGraphNode(-1);
+        UndirectedGraphNode b = new UndirectedGraphNode(1);
+        a.neighbors.add(b);
+        test.cloneGraph(a);
     }
 }
