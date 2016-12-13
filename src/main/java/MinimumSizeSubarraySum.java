d505ad11c43d72a8c6bc1690e0cdecb9746a7faa
bohan2345 <bohan.zheng@gmail.com>
2016-12-12 22:16:31 -0800

    lalala

--- src/main/java/MinimumSizeSubarraySum.java
+++ src/main/java/MinimumSizeSubarraySum.java
@@ -7,7 +7,7 @@
 public class MinimumSizeSubarraySum {
     /**
      * @param nums: an array of integers
-     * @param s: an integer
+     * @param target: an integer
      * @return: an integer representing the minimum size of subarray
      */
     public int minimumSize(int[] nums, int target) {
