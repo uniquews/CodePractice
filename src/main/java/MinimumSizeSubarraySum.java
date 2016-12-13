79d97b3ebfca1b503b08f33a242a0e6e919fd291
bohan2345 <bohan.zheng@gmail.com>
2016-12-12 22:16:00 -0800

    lalala

--- src/main/java/MinimumSizeSubarraySum.java
+++ src/main/java/MinimumSizeSubarraySum.java
@@ -7,33 +7,26 @@
 public class MinimumSizeSubarraySum {
     /**
      * @param nums: an array of integers
-     * @param target: an integer
+     * @param s: an integer
      * @return: an integer representing the minimum size of subarray
      */
     public int minimumSize(int[] nums, int target) {
-        // write your code here
         if (nums == null || nums.length == 0) {
             return -1;
         }
-
         boolean found = false;
-
         int right = 0, sum = 0, result = Integer.MAX_VALUE;
         for (int left = 0; left < nums.length; left++) {
-            while (right < nums.length) {
-                if (sum < target) {
-                    sum += nums[right];
-                    right++;
-                } else {
-                    found = true;
-                    result = Math.min(result, right - left);
-                    break;
-                }
+            while (right < nums.length && sum < target) {
+                sum += nums[right];
+                right++;
             }
-
-            sum -= nums[left];
+            if (sum >= target) {
+                found = true;
+                result = Math.min(result, right - left);
+                sum -= nums[left];
+            }
         }
-
         return found ? result : -1;
     }
 }
