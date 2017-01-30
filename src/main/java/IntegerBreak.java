b89157ac91bc945d197c6a3a0f266d073860e885
uniquews <wstjpu@gmail.com>
2017-01-29 19:56:50 -0500

    Integer Break

--- src/main/java/IntegerBreak.java
+++ src/main/java/IntegerBreak.java
@@ -1,6 +1,8 @@
 /**
  * Created by shuaiwang on 1/29/17.
  */
+
+// The idea is to multiply 3 all the times until you can't fo it
 public class IntegerBreak {
     public int integerBreak(int n) {
         if(n == 2) return 1;
@@ -12,6 +14,6 @@ public class IntegerBreak {
         }
         product *= n;
 
-        return product;
+        return product; // production = 3 * 3 * # *... * last number which could be [2,3,4]
     }
 }
