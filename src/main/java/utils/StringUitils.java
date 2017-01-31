4b76ab8c7eba22635849767350f4d9d7f9707149
bohan zheng <bohan zheng>
2017-01-30 21:02:40 -0800

    add string utils

--- src/main/java/utils/StringUitils.java
+++ src/main/java/utils/StringUitils.java
@@ -6,4 +6,20 @@ package utils;
  * @author Bohan Zheng
  */
 public class StringUitils {
+  public static String charArrayToString(char[] strs) {
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    for (char c : strs) {
+      if (c == 0) {
+        count++;
+      } else {
+        if (count != 0) {
+          sb.append(count);
+          count = 0;
+        }
+        sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
 }
