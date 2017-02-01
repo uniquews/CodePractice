1cc3c4d2971bc7e655124dd523849454711d985d
uniquews <wstjpu@gmail.com>
2017-02-01 00:30:53 -0500

    Expression Expand

---
+++ src/main/java/ExpressionExpand.java
@@ -0,0 +1,59 @@
+/**
+ * Created by shuaiwang on 1/31/17.
+ */
+public class ExpressionExpand {
+    public String expressionExpand(String s) {
+        // Write your code here
+        if (s == null || s.length() == 0 || s.charAt(0) == '0')
+            return "";
+        StringBuilder sb = new StringBuilder();
+        helper(s, 0, 1, sb);
+        return sb.toString();
+    }
+
+    private int helper(String s, int index, int times, StringBuilder sb) {
+        int endIndexOfSubString = index;
+        while (endIndexOfSubString < s.length() && s.charAt(endIndexOfSubString) != ']') {
+            endIndexOfSubString++;
+        }
+        while (times != 0) {
+            for (int i = index; i < s.length(); i++) {
+                if (s.charAt(i) == ']') {
+                    endIndexOfSubString = i;
+                    break;
+                } else if (!isCurrentNumber(s.charAt(i))) {
+                    sb.append(s.charAt(i));
+                } else {
+                    // first, we need to extact number
+                    int start = i, left = i + 1, right = i + 1;
+                    while (s.charAt(left) != '[') {
+                        left++;
+                    }
+                    int numbers = Integer.parseInt(s.substring(start, left));
+                    int end = helper(s,left+ 1, numbers, sb);
+                    i = end;
+                }
+            }
+            times--;
+        }
+        return endIndexOfSubString;
+    }
+
+    private boolean isCurrentNumber(char c) {
+        if (c >= '0' && c <= '9') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        ExpressionExpand test = new ExpressionExpand();
+        String s1 = "3[2[ad]3[pf]]xyz";
+        String s2 = "abc3[a]";
+        String s3 = "3[abc]";
+        String s4 = "4[ac]dy";
+        System.out.print(test.expressionExpand(s1));
+    }
+
+}
