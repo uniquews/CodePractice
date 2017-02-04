7743d9e120414c3f8fb29f04ac44ade20431fae6
uniquews <wstjpu@gmail.com>
2017-02-04 15:06:24 -0500

    sqrt(x)

---
+++ src/main/java/Sqrtx.java
@@ -0,0 +1,30 @@
+/**
+ * Created by shuaiwang on 2/4/17.
+ */
+public class Sqrtx {
+    public int sqrt(int x) {
+        // write your code here
+        if (x == 0) {
+            return 0;
+        }
+
+        int start = 1, end = x;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid < x) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (end * end == x) {
+            return end;
+        }
+
+        return start;
+    }
+}
