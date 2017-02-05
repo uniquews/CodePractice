d8d7e91463ef2aad9a6414928a81b3fdeb02dad7
uniquews <wstjpu@gmail.com>
2017-02-05 00:37:43 -0500

    Binary Tree Longest Consecutive Sequence

--- src/main/java/BinaryTreeLongestConsecutiveSequence.java
+++ src/main/java/BinaryTreeLongestConsecutiveSequence.java
@@ -7,58 +7,46 @@ import utils.TreeNode;
 public class BinaryTreeLongestConsecutiveSequence {
     class ResultType {
         public int ascendingLen;
-        public int descendingLen;
         public int maxAsLen;
-        public int maxDeLen;
 
-        public ResultType(int a, int d, int ma, int md) {
+        public ResultType(int a, int ma) {
             ascendingLen = a;
-            descendingLen = d;
             maxAsLen = ma;
-            maxDeLen = md;
         }
     }
     public int longestConsecutive(TreeNode root) {
         // Write your code here
-        return Math.max(divideAndConquor(root).maxAsLen, divideAndConquor(root).maxDeLen);
+        return divideAndConquor(root).maxAsLen;
     }
 
     private ResultType divideAndConquor(TreeNode n) {
         if (n == null) {
-            return new ResultType(0, 0, 0, 0);
+            return new ResultType(0, 0);
         }
 
         if (n.left == null && n.right == null) {
-            return new ResultType(1, 1, 1, 1);
+            return new ResultType(1, 1);
         }
 
         ResultType leftNode = divideAndConquor(n.left);
         ResultType rightNode = divideAndConquor(n.right);
 
-        ResultType current  = new ResultType(0,0,0, 0);
+        ResultType current  = new ResultType(1,0);
         if (n.left != null && n.val == n.left.val - 1) {
             current.ascendingLen = leftNode.ascendingLen + 1;
         }
 
-        if (n.left != null && n.val == n.left.val + 1) {
-            current.descendingLen = leftNode.descendingLen + 1;
-        }
 
         if (n.right != null && n.val == n.right.val - 1) {
             current.ascendingLen = rightNode.ascendingLen + 1;
         }
 
-        if (n.right != null && n.val == n.right.val + 1) {
-            current.descendingLen = rightNode.descendingLen + 1;
-        }
-
         current.maxAsLen = Math.max(Math.max(current.ascendingLen, leftNode.maxAsLen), rightNode.maxAsLen);
-        current.maxDeLen = Math.max(Math.max(current.descendingLen, leftNode.maxDeLen), rightNode.maxDeLen);
         return current;
     }
 
     public static void main(String[] args) {
         BinaryTreeLongestConsecutiveSequence test = new BinaryTreeLongestConsecutiveSequence();
-        test.longestConsecutive(BinaryTreeUtils.deserialize("1,#,3,2,4,#,#,#,5"));
+        test.longestConsecutive(BinaryTreeUtils.deserialize("2,#,3,2,#,1"));
     }
 }
