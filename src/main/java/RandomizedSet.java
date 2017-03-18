da1dbe9a497ce5d77678fe459f796f79ce672372
uniquews <wstjpu@gmail.com>
2017-03-18 12:00:12 -0400

    Insert Delete GetRandom O(1)

--- src/main/java/RandomizedSet.java
+++ src/main/java/RandomizedSet.java
@@ -6,14 +6,19 @@ import java.util.Random;
 /**
  * Created by shuaiwang on 3/17/17.
  */
+
+// 容易错的地方是，remove function里，在list中swap 两个数的同时需要更新map里面对应的index
+// remove必须要和数组末尾swap 否则删除就不是O(1)
+// 不理解这种getRandom 如何保证Each element must have the same probability of being returned.
+
 public class RandomizedSet {
+
     List<Integer> list;
     HashMap<Integer, Integer> map;
     int count;
     public RandomizedSet() {
         list = new ArrayList<>();
         map = new HashMap<>();
-        count = 0;
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
@@ -22,7 +27,6 @@ public class RandomizedSet {
             return false;
         }
 
-        count = 0;
         int pos = list.size();
         list.add(val);
         map.put(val, pos);
@@ -35,32 +39,20 @@ public class RandomizedSet {
             return false;
         }
 
-        count = 0;
         int pos = map.get(val);
-
-        int lastIndex = list.size() - 1;
-        list.set(pos, list.get(lastIndex));
-        list.set(lastIndex, val);
-
+        int tmp = list.get(list.size() - 1);
+        list.set(list.size() - 1, val);
+        list.set(pos, tmp);
+        map.put(tmp, pos);
         map.remove(val);
-        map.put(list.get(pos), pos);
-        list.remove(lastIndex);
+        list.remove(list.size() - 1);
         return true;
     }
 
     /** Get a random element from the set. */
     public int getRandom() {
-        int size = list.size();
-        if (count == size) {
-            count = 0;
-        }
-        int endPosition = size - count;
-        Random random = new Random();
-        int peekPosition = random.nextInt(endPosition);
-        int result = list.get(peekPosition);
-        list.set(peekPosition, list.get(endPosition - 1));
-        list.set(endPosition - 1, result);
-        count++;
-        return result;
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
     }
 }
+
