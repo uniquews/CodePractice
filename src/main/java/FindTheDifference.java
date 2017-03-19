75ab18889eaead157bb3ef3b0951d897de95d83a
uniquews <wstjpu@gmail.com>
2017-03-18 22:56:35 -0400

    Find the difference

--- src/main/java/FindTheDifference.java
+++ src/main/java/FindTheDifference.java
@@ -2,6 +2,7 @@
  * Created by shuaiwang on 3/18/17.
  */
 public class FindTheDifference {
+    //思路： 用t中所有的char 去抵消s中所有的char，剩下的就是结果
     public char findTheDifference(String s, String t) {
         int asciiCharDelta = (int)t.charAt(t.length() - 1);
         for (int i = 0; i < s.length(); i++) {
