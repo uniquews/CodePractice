426c53c662ca4a3dd7894ad1a865c0d1f2ac193f
uniquews <wstjpu@gmail.com>
2017-03-18 21:17:22 -0400

    Paint Fence

--- src/main/java/PaintFence.java
+++ src/main/java/PaintFence.java
@@ -42,6 +42,9 @@ public class PaintFence {
 
     //
     public int numWays(int n, int k) {
+        if (n == 0 || k == 0) {
+            return 0;
+        }
         if (n == 1) {
             return k;
         }
@@ -51,8 +54,9 @@ public class PaintFence {
         int totalNumOfWaySame = k;
 
         for (int i = 3; i <= n; i++) {
+            int tmp = totalNumOfWayDiff;
             totalNumOfWayDiff = (k - 1) * (totalNumOfWayDiff + totalNumOfWaySame);
-            totalNumOfWaySame = totalNumOfWayDiff;
+            totalNumOfWaySame = tmp;
         }
         return totalNumOfWayDiff + totalNumOfWaySame;
     }
