bf628b507838cdcfce0b30ffa43ac01371942c8a
uniquews <wstjpu@gmail.com>
2017-04-19 22:18:50 -0400

    ugly number

--- src/main/java/UglyNumberII.java
+++ src/main/java/UglyNumberII.java
@@ -4,7 +4,14 @@ import java.util.List;
 /**
  * Created by shuaiwang on 10/23/16.
  * <p>
- * i, j, k are three pointers. Every time you picke the smallest one as current ugly number
+ * i, j, k are three pointers. Every time you pick the smallest one as current ugly number
+ *
+ * 关键思路是这句话：
+     The key is how to maintain the order of the ugly numbers.
+     Try a similar approach of merging from three sorted lists: L1, L2, and L3.
+     (1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+     (2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+     (3) 1×5, 2×5, 3×5, 4×5, 5×5, …
  */
 public class UglyNumberII {
     public int nthUglyNumber(int n) {
@@ -18,7 +25,7 @@ public class UglyNumberII {
             int m3 = result.get(j) * 3;
             int m5 = result.get(k) * 5;
 
-            int cur = Math.min(m2, Math.min(m2, m5));
+            int cur = Math.min(m2, Math.min(m3, m5));
             result.add(cur);
 
             if (cur == m2) {
