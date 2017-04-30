e4cafeec62478d7d17d55f068459058908f1d763
bohan2345 <bohan.zheng@gmail.com>
2017-04-29 20:17:44 -0700

    s

---
+++ src/main/java/b/ConvexPolygon.java
@@ -0,0 +1,32 @@
+package b;
+
+/**
+ * Created by bohan on 4/29/17.
+ */
+public class ConvexPolygon {
+    public boolean isConvex(int[][] points) {
+        int n = points.length;
+        long prevNormal = 0; // 法向量 = normal, is a vector of perpendicular of AB x BC.
+        for (int i = 0; i < n; i++) {
+            int[] currentPoint = points[i];
+            int[] prevPoint = points[(i - 1 + n) % n];
+            int[] nextPoint = points[(i + 1 + n) % n];
+            long currentNormal = getNormal(currentPoint, prevPoint, nextPoint);
+            if (!(currentNormal == 0 || currentNormal * prevNormal >= 0)) {
+                return false;
+            }
+            prevNormal = currentNormal;
+        }
+        return true;
+    }
+
+    private long getNormal(int[] current, int[] prev, int[] next) {
+        // prev - current.
+        int dx1 = prev[0] - current[0];
+        int dy1 = prev[1] - current[1];
+        // next - current.
+        int dx2 = next[0] - current[0];
+        int dy2 = next[1] - current[1];
+        return dx1 * dy2 - dx2 * dy1;
+    }
+}
