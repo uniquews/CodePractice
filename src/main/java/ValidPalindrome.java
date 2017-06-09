a28f019dbdc67f4fbf16783c00b2b92226db0f1a
uniquews <wstjpu@gmail.com>
2017-06-08 21:22:32 -0400

    .

---
+++ src/main/java/ValidPalindrome.java
@@ -0,0 +1,36 @@
+/**
+ * Created by shuaiwang on 6/8/17.
+ */
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        // Write your code here
+        if (s == null || s.length() == 0)
+            return true;
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            while (start < end && isValid(s.charAt(start)))
+                start++;
+
+            if (start == end)
+                return true;
+
+            while (start < end && isValid(s.charAt(end)))
+                end--;
+
+            if (Character.toLowerCase(s.charAt(start)) == Character.toLowerCase(s.charAt(end))) {
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+}
