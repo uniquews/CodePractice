d583d394943020aa4e8dfe5c318f1c855d4fd9e6
uniquews <wstjpu@gmail.com>
2017-06-11 12:03:51 -0400

    .

--- src/main/java/HashFunction.java
+++ src/main/java/HashFunction.java
@@ -5,20 +5,25 @@ public class HashFunction {
 
     // Solution 2
 
+
     /**
+     * (a×b) mod c=(a mod c * b mod c) mod c
+     * (a+b) mod c=(a mod c+ b mod c) mod c
+     *
+     *
      * f(n) = (a * 33^2 + b * 33 + c) % k
      * => ((a * 33^2 + b * 33) % k + c % k) % k
      * => ((33 * (a * 33 + b)) % k + c % k) % k
      * => (((33 % k) * (a * 33 + b) % k) % k + c % k) % k
      * => (((33 % k) * f(n - 1)) % k + c % k) % k
-     * => (((33 % k) * f(n - 1)) + c ) % k
+     * => ((33 * f(n - 1)) % k + c % k) % k
+     * => (33 * f(n - 1) + c) % k
      */
     public int hashCode(char[] key, int k) {
         // write your code here
         long result = 0;
         for (int i = 0; i < key.length; i++) {
-//            result = (((33 % k) * result) % k + key[i] % k) % k;
-            result = ((33 % k) * result + key[i]) % k;
+            result = (33 * result + key[i]) % k;
         }
         return (int) result;
     }
