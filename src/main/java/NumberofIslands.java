4cd149d4b52fa7ad9d6995b85a616d052a3d0176
uniquews <wstjpu@gmail.com>
2017-06-17 16:52:37 -0400

    .

--- src/main/java/NumberofIslands.java
+++ src/main/java/NumberofIslands.java
@@ -2,6 +2,8 @@
  * Created by shuaiwang on 11/29/16.
  */
 public class NumberofIslands {
+    int[] x = {-1, 1, 0, 0};
+    int[] y = {0, 0, -1, 1};
     public int numIslands(boolean[][] grid) {
         // Write your code here
         int result = 0;
@@ -16,17 +18,17 @@ public class NumberofIslands {
         return result;
     }
 
-    private void dfs(boolean[][] grid, int row, int column) {
-        if (row < 0 || column < 0 || row == grid.length || column == grid[0].length) {
+    private void dfs(boolean[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row == grid.length || col == grid[0].length) {
             return;
         }
 
-        if (grid[row][column]) {
-            grid[row][column] = false;
-            dfs(grid, row + 1, column);
-            dfs(grid, row, column + 1);
-            dfs(grid, row - 1, column);
-            dfs(grid, row, column - 1);
+        if (grid[row][col] == false)
+            return;
+
+        grid[row][col] = false;
+        for (int i = 0; i < x.length; i++) {
+            dfs(grid, row + x[i], col + y[i]);
         }
     }
 }
