86047894657dea565fa5c94e2c35f11c2d81e041
uniquews <wstjpu@gmail.com>
2017-06-20 22:50:49 -0400

    。

--- src/main/java/Pow.java
+++ src/main/java/Pow.java
@@ -3,29 +3,36 @@
  */
 public class Pow {
     public double myPow(double x, int n) {
-        if (n >= 0) {
-            return power(x, n);
-        } else {
-            return 1 / power(x, n);
+        if (n == 1) {
+            return x;
         }
-    }
 
-    private double power(double x, int n) {
-        if (n == 0)
+        if (n == 0) {
             return 1.0;
+        }
 
-        double v = power(x, n / 2);
-        if (n % 2 == 0) {
-            return v * v;
+        if (n < 0) {
+            double tmp = myPow(x, (-1) * n / 2);
+            if (n % 2 == 0) {
+                return 1 / tmp * tmp;
+            } else {
+                System.out.println(x * tmp * tmp);
+                return 1 / (x * tmp * tmp);
+            }
         } else {
-            return v * v * x;
+            double tmp = myPow(x, n / 2);
+            if (n % 2 == 0) {
+                return tmp * tmp;
+            } else {
+                return x * tmp * tmp;
+            }
         }
     }
 
     public static void main(String[] args) {
-        double x = 8.0;
-        int n = 3;
+        double x = 2.0;
+        int n = -3;
         Pow p = new Pow();
-        p.myPow(x, n);
+        System.out.println(p.myPow(x, n));
     }
 }
