83bf1bf1e92e0dbfd66e6e7c2fa22e3404ec47ef
uniquews <wstjpu@gmail.com>
2017-07-11 23:11:22 -0400

    .

---
+++ src/main/java/WordPattern.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Created by shuaiwang on 7/11/17.
+ */
+public class WordPattern {
+    /**
+     * Integer 超过128就不能用== 必须用Objects.equals
+     * */
+    public boolean wordPattern(String pattern, String str) {
+        String[] arr = str.split(" ");
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        if (pattern.length() != arr.length)
+            return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!Objects.equals(map1.put(pattern.charAt(i), i), map2.put(arr[i], i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        WordPattern test = new WordPattern();
+//        String p ="ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdd";
+//        String p ="cdd";
+//        String a = "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t";
+//        String a = "s t t";
+
+//        test.wordPattern(p, a);
+        Integer a = -215;
+        Integer b = -215;
+        if (a == b)
+            System.out.println("***");
+
+
+    }
+}
