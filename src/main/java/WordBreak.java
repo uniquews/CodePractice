5e32bda7452eaa99bd220ddb422e4423b4e072bd
uniquews <wstjpu@gmail.com>
2017-07-11 21:02:44 -0400

    .

--- src/main/java/WordBreak.java
+++ src/main/java/WordBreak.java
@@ -1,31 +1,55 @@
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by shuaiwang on 8/30/16.
  */
 public class WordBreak {
-    public boolean wordBreak(String s, Set<String> dict) {
-        // write your code here
-        if (s == null) {
-            return false;
+//    public boolean wordBreak(String s, Set<String> dict) {
+//        // write your code here
+//        if (s == null) {
+//            return false;
+//        }
+//
+//        boolean[] f = new boolean[s.length() + 1];
+//        f[0] = true;
+//
+//        int maxLength = getMaxLength(dict);
+//
+//        for (int i = 1; i <= s.length(); i++) {
+//            for (int j = i - 1; j >= 0; j--) {
+//                if (i - j > maxLength) {
+//                    break;
+//                }
+//                String tmp = s.substring(j, i);
+//                f[i] = f[i] || f[j] && dict.contains(tmp);
+//            }
+//        }
+//
+//        return f[s.length()];
+//    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>();
+
+        for (String str : wordDict) {
+            dict.add(str);
         }
 
-        boolean[] f = new boolean[s.length() + 1];
-        f[0] = true;
-
-        int maxLength = getMaxLength(dict);
-
+        boolean [][] f = new boolean[s.length()][s.length()];
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (i - j > maxLength) {
-                    break;
+            for (int j = 0; j <= s.length() - i; j++) {
+                String cur = s.substring(j, j + i);
+                if (dict.contains(cur)) {
+                    f[j][j + i - 1] = true;
                 }
-                String tmp = s.substring(j, i);
-                f[i] = f[i] || f[j] && dict.contains(tmp);
+                if (i >= 2) {
+                    for (int k = 1; k < i; k++) {
+                        f[j][i - 1] = f[j][i - 1] || (f[j][k - 1] && f[k][j + i - 1]);
+                    }
+                }
             }
         }
-
-        return f[s.length()];
+        return f[0][s.length() - 1];
     }
 
     private int getMaxLength(Set<String> dict) {
@@ -34,5 +58,12 @@ public class WordBreak {
             maxLength = Math.max(maxLength, word.length());
         }
         return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "ab";
+        List<String> dict = new ArrayList<>(Arrays.asList("a","b"));
+        WordBreak test = new WordBreak();
+        test.wordBreak(s, dict);
     }
 }
