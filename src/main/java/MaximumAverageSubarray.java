e434184a1a74c3de16aa933453512faae5c14d2d
uniquews <wstjpu@gmail.com>
2017-07-31 23:23:52 -0400

    .

--- src/main/java/MaximumAverageSubarray.java
+++ src/main/java/MaximumAverageSubarray.java
@@ -37,6 +37,46 @@ public class MaximumAverageSubarray {
         return false;
     }
 
+//    public double findMaxAverage(int[] nums, int k) {
+//        double end = Integer.MIN_VALUE;
+//        double start = Integer.MAX_VALUE;
+//
+//        for (int n : nums) {
+//            end = Math.max(n, end);
+//            start = Math.min(n, start);
+//        }
+//
+//        while (end - start >= 1e-6) {
+//            double mid = start + (end - start) / 2;
+//            boolean aboveOrBelowAvg = checkAvg(nums, k, mid);
+//            if (aboveOrBelowAvg) {
+//                start = mid;
+//            } else {
+//                end = mid;
+//            }
+//        }
+//
+//        return start;
+//    }
+//
+//    private boolean checkAvg(int[] nums, int k, double avg) {
+//        double[] sum = new double[nums.length + 1];
+//        double minInPrevious = 0.0;
+//        for (int i = 1; i <= nums.length; i++) {
+//            sum[i] = sum[i - 1] + nums[i - 1] * 1.0 - avg;
+//            if (i >= k) {
+//                if (sum[i] - minInPrevious >= 0) {
+//                    return true;
+//                }
+//            }
+//
+//            if (i >= k) {
+//                minInPrevious = Math.min(minInPrevious, sum[i - k + 1]);
+//            }
+//        }
+//        return false;
+//    }
+
     public static void main(String[] args) {
         int[] nums = {1,2,3};
         int k = 3;
