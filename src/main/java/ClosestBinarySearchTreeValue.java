71c123970c32fbd6bca94854957cde7e3567f9e3
uniquews <wstjpu@gmail.com>
2017-08-09 22:23:41 -0400

    .

--- src/main/java/ClosestBinarySearchTreeValue.java
+++ src/main/java/ClosestBinarySearchTreeValue.java
@@ -4,24 +4,42 @@ import utils.TreeNode;
  * Created by shuaiwang on 3/30/17.
  */
 public class ClosestBinarySearchTreeValue {
-    public int closestValue(TreeNode root, double target) {
-        if (root == null) {
-            return Integer.MAX_VALUE;
-        }
+//    public int closestValue(TreeNode root, double target) {
+//        if (root == null) {
+//            return Integer.MAX_VALUE;
+//        }
+//
+//        TreeNode next;
+//        if (root.val > target) {
+//            if (root.left == null) {
+//                return root.val;
+//            }
+//            next = root.left;
+//        } else {
+//            if (root.right == null) {
+//                return root.val;
+//            }
+//            next = root.right;
+//        }
+//        int closetNode = closestValue(next, target);
+//        return Math.abs(root.val - target) > Math.abs(closetNode - target) ? root.val : closetNode;
+//    }
 
-        TreeNode next;
-        if (root.val > target) {
+    public int closestValue(TreeNode root, double target) {
+        if (root.val >= target) {
             if (root.left == null) {
                 return root.val;
+            } else {
+                int fromLeft = closestValue(root.left, target);
+                return Math.abs(root.val - target) > Math.abs(fromLeft - target) ? fromLeft : root.val;
             }
-            next = root.left;
         } else {
             if (root.right == null) {
                 return root.val;
+            } else {
+                int fromRight = closestValue(root.right, target);
+                return Math.abs(root.val - target) >  Math.abs(fromRight - target) ? fromRight : root.val;
             }
-            next = root.right;
         }
-        int closetNode = closestValue(next, target);
-        return Math.abs(root.val - target) > Math.abs(closetNode - target) ? root.val : closetNode;
     }
 }
