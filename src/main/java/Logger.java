d9bd00f7788241411a85414fd01bd12bdde11f11
uniquews <wstjpu@gmail.com>
2017-08-13 19:58:06 -0400

    .

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -1,31 +1,29 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by shuaiwang on 1/12/17.
  */
 public class Logger {
     /** Initialize your data structure here. */
-    HashMap<String, Integer> hashMap;
+    Map<String, Integer> map;
     public Logger() {
-        hashMap = new HashMap<>();
+        map = new HashMap<>();
     }
 
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
      If this method returns false, the message will not be printed.
      The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if (hashMap.containsKey(message)) {
-            int previousTime = hashMap.get(message);
-            int gap = timestamp - previousTime;
-            if (gap < 10) {
-                return false;
-            } else {
-                hashMap.put(message, timestamp);
-                return true;
-            }
-        } else {
-            hashMap.put(message, timestamp);
+        if (!map.containsKey(message)) {
+            map.put(message, timestamp);
             return true;
         }
+
+        if (timestamp >= map.get(message) + 10) {
+            map.put(message, timestamp);
+            return true;
+        } else
+            return false;
     }
 }
