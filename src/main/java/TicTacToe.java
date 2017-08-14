cb5e121934744bd72069fce83a4300429d2583c4
uniquews <wstjpu@gmail.com>
2017-08-13 23:00:20 -0400

    .

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -2,20 +2,80 @@
  * Created by shuaiwang on 1/27/17.
  */
 public class TicTacToe {
-    /** Initialize your data structure here. */
+//    /** Initialize your data structure here. */
+//
+//    public int[] pointsInRow;
+//    public int[] pointsInColumn;
+//    public int pointsInleftDiagonal;
+//    public int pointsInrightDiagonal;
+//    private int size;
+//
+//    public TicTacToe(int n) {
+//        pointsInRow = new int[n];
+//        pointsInColumn = new int[n];
+//        pointsInleftDiagonal = 0;
+//        pointsInrightDiagonal = 0;
+//        size = n;
+//    }
+//
+//    /** Player {player} makes a move at ({row}, {col}).
+//     @param row The row of the board.
+//     @param col The column of the board.
+//     @param player The player, can be either 1 or 2.
+//     @return The current winning condition, can be either:
+//     0: No one wins.
+//     1: Player 1 wins.
+//     2: Player 2 wins. */
+//    public int move(int row, int col, int player) {
+//        if (player == 1) {
+//            pointsInRow[row]++;
+//            pointsInColumn[col]++;
+//
+//            if (row == col) {
+//                pointsInleftDiagonal++;
+//            }
+//
+//            if (row + col == size - 1) {
+//                pointsInrightDiagonal++;
+//            }
+//        } else {
+//            pointsInRow[row]--;
+//            pointsInColumn[col]--;
+//
+//            if (row == col) {
+//                pointsInleftDiagonal--;
+//            }
+//
+//            if (row + col == size - 1) {
+//                pointsInrightDiagonal--;
+//            }
+//        }
+//
+//        if (pointsInRow[row] == size || pointsInColumn[col] == size
+//            || pointsInleftDiagonal == size
+//            || pointsInrightDiagonal ==size) {
+//            return 1;
+//        }
+//
+//        if (pointsInRow[row] == (-1) * size || pointsInColumn[col] == (-1) * size
+//                || pointsInleftDiagonal == (-1) * size
+//                || pointsInrightDiagonal == (-1) * size) {
+//            return 2;
+//        }
+//        return 0;
+//    }
 
-    public int[] pointsInRow;
-    public int[] pointsInColumn;
-    public int pointsInleftDiagonal;
-    public int pointsInrightDiagonal;
-    private int size;
+    /** Initialize your data structure here. */
+    public int[] rows;
+    public int[] cols;
+    public int leftDia;
+    public int rightDia;
 
     public TicTacToe(int n) {
-        pointsInRow = new int[n];
-        pointsInColumn = new int[n];
-        pointsInleftDiagonal = 0;
-        pointsInrightDiagonal = 0;
-        size = n;
+        rows = new int[n];
+        cols = new int[n];
+        leftDia = 0;
+        rightDia = 0;
     }
 
     /** Player {player} makes a move at ({row}, {col}).
@@ -28,40 +88,38 @@ public class TicTacToe {
      2: Player 2 wins. */
     public int move(int row, int col, int player) {
         if (player == 1) {
-            pointsInRow[row]++;
-            pointsInColumn[col]++;
-
+            rows[row]++;
+            cols[col]++;
             if (row == col) {
-                pointsInleftDiagonal++;
+                leftDia++;
             }
-
-            if (row + col == size - 1) {
-                pointsInrightDiagonal++;
+            if (row + col == rows.length - 1) { // 易错！！ 不要写成else if
+                rightDia++;
             }
         } else {
-            pointsInRow[row]--;
-            pointsInColumn[col]--;
-
+            rows[row]--;
+            cols[col]--;
             if (row == col) {
-                pointsInleftDiagonal--;
+                leftDia--;
             }
-
-            if (row + col == size - 1) {
-                pointsInrightDiagonal--;
+            if (row + col == rows.length - 1) {
+                rightDia--;
             }
         }
 
-        if (pointsInRow[row] == size || pointsInColumn[col] == size
-            || pointsInleftDiagonal == size
-            || pointsInrightDiagonal ==size) {
+        if (rows[row] == rows.length || cols[col] == cols.length || leftDia == rows.length || rightDia == rows.length)
             return 1;
-        }
-
-        if (pointsInRow[row] == (-1) * size || pointsInColumn[col] == (-1) * size
-                || pointsInleftDiagonal == (-1) * size
-                || pointsInrightDiagonal == (-1) * size) {
+        else if (rows[row] == (-1) * rows.length || cols[col] == (-1) * cols.length || leftDia == (-1) * rows.length || rightDia == (-1) * rows.length) {
             return 2;
+        } else {
+            return 0;
         }
-        return 0;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe test = new TicTacToe(3);
+        test.move(1,1,2);
+        test.move(0,2,2);
+        test.move(2,0,2);
     }
 }
