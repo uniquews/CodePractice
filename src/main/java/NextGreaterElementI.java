9010b6dffa89290f01c30dc0e95f75b174b4ee8e
uniquews <wstjpu@gmail.com>
2017-08-20 01:10:38 -0400

    .

--- src/main/java/NextGreaterElementI.java
+++ src/main/java/NextGreaterElementI.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -6,27 +7,52 @@ import java.util.Stack;
  * Created by shuaiwang on 4/30/17.
  */
 public class NextGreaterElementI {
-    public int[] nextGreaterElement(int[] findNums, int[] nums) {
-        Stack<Integer> stk = new Stack<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int i = 0;
-        while (i < nums.length) {
-            if (stk.isEmpty() || nums[stk.peek()] > nums[i]) {
-                stk.push(i);
-                i++;
-            } else {
-                int smallIndex = stk.pop();
-                map.put(nums[smallIndex], nums[i]);
-            }
-        }
+//    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+//        Stack<Integer> stk = new Stack<>();
+//        Map<Integer, Integer> map = new HashMap<>();
+//        int i = 0;
+//        while (i < nums.length) {
+//            if (stk.isEmpty() || nums[stk.peek()] > nums[i]) {
+//                stk.push(i);
+//                i++;
+//            } else {
+//                int smallIndex = stk.pop();
+//                map.put(nums[smallIndex], nums[i]);
+//            }
+//        }
+//
+//        int[] result = new int[findNums.length];
+//        int index = 0;
+//        for (int n : findNums) {
+//            if (map.containsKey(n)) {
+//                result[index++] = map.get(n);
+//            } else {
+//                result[index++] = -1;
+//            }
+//        }
+//        return result;
+//    }
 
-        int[] result = new int[findNums.length];
-        int index = 0;
-        for (int n : findNums) {
-            if (map.containsKey(n)) {
-                result[index++] = map.get(n);
+    public int[] nextGreaterElements(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < nums.length * 2; i++) {
+            if (i < nums.length) {
+                if (stk.isEmpty() || nums[stk.peek()] > nums[i]) {
+                    stk.push(i);
+                } else {
+                    while (nums[stk.peek()] < nums[i]) {
+                        int index = stk.pop();
+                        result[index] = nums[i];
+                    }
+                    stk.push(i);
+                }
             } else {
-                result[index++] = -1;
+                while (nums[stk.peek()] < nums[i % nums.length]) {
+                    result[stk.pop()] = nums[i % nums.length];
+                }
             }
         }
         return result;
