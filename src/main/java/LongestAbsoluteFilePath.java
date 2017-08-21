529f187aaa503b48e6c66dba170418166139e145
uniquews <wstjpu@gmail.com>
2017-08-20 22:53:31 -0400

    .

--- src/main/java/LongestAbsoluteFilePath.java
+++ src/main/java/LongestAbsoluteFilePath.java
@@ -4,39 +4,69 @@ import java.util.Stack;
  * Created by shuaiwang on 2/22/17.
  */
 public class LongestAbsoluteFilePath {
+//    public int lengthLongestPath(String input) {
+//        if (input == null || input.length() == 0) {
+//            return 0;
+//        }
+//
+//        int currentLen = 0;
+//        int result = 0;
+//        Stack<Integer> stk = new Stack<>();
+//        String[] A = input.split("\n");
+//
+//        for (int i = 0; i < A.length; i++) {
+//            int level = countTab(A[i]);
+//            if (stk.size() > level) {
+//                while (stk.size() > level) {
+//                    currentLen -= stk.pop();
+//                }
+//            }
+//            if (isFile(A[i])) {
+//                result = Math.max(result, currentLen + A[i].length() - level);
+//            } else {
+//                stk.push(A[i].length() - level + 1);
+//                currentLen += stk.peek();
+//            }
+//        }
+//        return result;
+//    }
+//
+//    private int countTab(String str) {
+//        return str.lastIndexOf("\t") + 1;
+//    }
+//
+//    private boolean isFile(String str) {
+//        return str.contains(".");
+//    }
+
     public int lengthLongestPath(String input) {
         if (input == null || input.length() == 0) {
             return 0;
         }
 
-        int currentLen = 0;
+        String[] s = input.split("\n");
+        Stack<String> stk = new Stack<>();
         int result = 0;
-        Stack<Integer> stk = new Stack<>();
-        String[] A = input.split("\n");
+        int len = 0;
 
-        for (int i = 0; i < A.length; i++) {
-            int level = countTab(A[i]);
-            if (stk.size() > level) {
-                while (stk.size() > level) {
-                    currentLen -= stk.pop();
-                }
+        for (int i = 0; i < s.length; i++) {
+            int tab = s[i].lastIndexOf('\t');
+            int realLen = s[i].length() - tab - 1;
+
+            while (!stk.isEmpty() && stk.peek().lastIndexOf('\t') >= tab) {
+                String prev = stk.pop();
+                int realLenInPrev = prev.length() - prev.lastIndexOf('\t') - 1;
+                len -= realLenInPrev;
             }
-            if (isFile(A[i])) {
-                result = Math.max(result, currentLen + A[i].length() - level);
-            } else {
-                stk.push(A[i].length() - level + 1);
-                currentLen += stk.peek();
+
+            if (s[i].indexOf('.') == -1) { // dir
+                stk.push(s[i]);
+                len += realLen;
+            } else { // file
+                result = Math.max(result, len + realLen + stk.size()); // add number of /
             }
         }
         return result;
-    }
-
-    private int countTab(String str) {
-        return str.lastIndexOf("\t") + 1;
-    }
-
-    private boolean isFile(String str) {
-        return str.contains(".");
     }
 
     public static void main(String[] args) {
