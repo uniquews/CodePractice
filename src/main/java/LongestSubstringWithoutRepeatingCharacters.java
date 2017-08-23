ca5c4850bfc7073c55cb105f2feacdf6681b7dcd
uniquews <wstjpu@gmail.com>
2017-08-22 21:52:27 -0400

    .

--- src/main/java/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,37 +1,24 @@
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by shuaiwang on 12/13/16.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
-        // write your code here
-        int result = 0;
         if (s == null || s.length() == 0) {
-            return result;
+            return 0;
         }
 
-        HashSet hashSet = new HashSet<>();
-
-        int left = 0;
-        int right = left;
-        for (; left < s.length(); left++) {
-            while (right < s.length() && !hashSet.contains(s.charAt(right))) {
-                hashSet.add(s.charAt(right));
+        Set<Character> set = new HashSet<>();
+        int right = 0, result = 0;
+        for (int left = 0; left < s.length(); left++) {
+            while (right < s.length() && !set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
                 right++;
             }
-
             result = Math.max(result, right - left);
-
-            if (right < s.length()) {
-                char dup = s.charAt(right);
-
-                while (s.charAt(left) != dup) {
-                    hashSet.remove(s.charAt(left));
-                    left++;
-                }
-                hashSet.remove(s.charAt(right));
-            }
+            set.remove(s.charAt(left));
         }
         return result;
     }
