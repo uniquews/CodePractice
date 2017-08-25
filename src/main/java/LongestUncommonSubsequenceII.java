6eaf1a12b1066925756de23764e77420eb077f6b
uniquews <wstjpu@gmail.com>
2017-08-24 21:18:41 -0400

    .

--- src/main/java/LongestUncommonSubsequenceII.java
+++ src/main/java/LongestUncommonSubsequenceII.java
@@ -5,22 +5,57 @@ import java.util.Comparator;
  * Created by shuaiwang on 5/1/17.
  */
 public class LongestUncommonSubsequenceII {
+//    public int findLUSlength(String[] strs) {
+//        int result = -1;
+//        for (int i = 0; i < strs.length; i++) {
+//            boolean isSubForAnyOtherStr = false;
+//            for (int j = 0; j < strs.length; j++) {
+//                if (i == j)
+//                    continue;
+//                isSubForAnyOtherStr = isSubForAnyOtherStr || isSub(strs[i], strs[j]);
+//            }
+//            if (!isSubForAnyOtherStr)
+//                result = Math.max(result, strs[i].length());
+//        }
+//        return result;
+//    }
+//
+//    private boolean isSub(String s, String t) {
+//        int i = 0, j = 0;
+//        while (i < s.length() && j < t.length()) {
+//            if (s.charAt(i) == t.charAt(j)) {
+//                i++;
+//                j++;
+//            } else {
+//                j++;
+//            }
+//        }
+//        if (i == s.length()) {
+//            return true;
+//        }
+//        return false;
+//    }
+
     public int findLUSlength(String[] strs) {
         int result = -1;
         for (int i = 0; i < strs.length; i++) {
-            boolean isSubForAnyOtherStr = false;
+            boolean isSub = false;
             for (int j = 0; j < strs.length; j++) {
                 if (i == j)
                     continue;
-                isSubForAnyOtherStr = isSubForAnyOtherStr || isSub(strs[i], strs[j]);
+                if (isSubsequence(strs[i], strs[j])) {
+                    isSub = true;
+                    break;
+                }
             }
-            if (!isSubForAnyOtherStr)
+            if (!isSub) {
                 result = Math.max(result, strs[i].length());
+            }
         }
         return result;
     }
 
-    private boolean isSub(String s, String t) {
+    private boolean isSubsequence(String s, String t) {
         int i = 0, j = 0;
         while (i < s.length() && j < t.length()) {
             if (s.charAt(i) == t.charAt(j)) {
