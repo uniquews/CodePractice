da735d37ae075bc977cf7a8f718de3db6c3cb9e5
uniquews <wstjpu@gmail.com>
2017-09-04 16:07:39 -0400

    .

--- src/main/java/InorderSuccessorInBinarySearchTree.java
+++ src/main/java/InorderSuccessorInBinarySearchTree.java
@@ -4,32 +4,51 @@ import utils.TreeNode;
  * Created by shuaiwang on 7/28/16.
  */
 public class InorderSuccessorInBinarySearchTree {
+//    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+//        // write your code here
+//        if (root == null) {
+//            return null;
+//        }
+//
+//        TreeNode result = null;
+//        while (root != null && root.val != p.val) {
+//            if (root.val > p.val) {
+//                result = root;
+//                root = root.left;
+//            } else {
+//                root = root.right; // small and not equal
+//            }
+//        }
+//
+//        if (root.right == null) {
+//            return result;
+//        }
+//
+//        root = root.right;
+//        while (root.left != null) {
+//            root = root.left;
+//        }
+//
+//        return root;
+//
+//    }
+
+    private TreeNode last = null;
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        // write your code here
+        helper(root, p);
+        return last;
+    }
+
+    private void helper(TreeNode root, TreeNode p) {
         if (root == null) {
-            return null;
+            return;
         }
 
-        TreeNode result = null;
-        while (root != null && root.val != p.val) {
-            if (root.val > p.val) {
-                result = root;
-                root = root.left;
-            } else {
-                root = root.right; // small and not equal
-            }
+        if (root.val <= p.val) {
+            helper(root.right, p);
+        } else if (root.val > p.val) {
+            last = root;
+            helper(root.left, p);
         }
-
-        if (root.right == null) {
-            return result;
-        }
-
-        root = root.right;
-        while (root.left != null) {
-            root = root.left;
-        }
-
-        return root;
-
     }
 }
