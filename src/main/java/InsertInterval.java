c2dc046e272eaec73935f57f2758d5eb7f139173
uniquews <wstjpu@gmail.com>
2017-09-05 21:14:37 -0400

    .

--- src/main/java/InsertInterval.java
+++ src/main/java/InsertInterval.java
@@ -9,18 +9,16 @@ import java.util.List;
 public class InsertInterval {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> result = new ArrayList<>();
-
         int i = 0;
         while (i < intervals.size() && intervals.get(i).end < newInterval.start) {
-            result.add(intervals.get(i));
-            i++;
+            result.add(intervals.get(i++));
         }
 
         result.add(newInterval);
-        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
-            Interval in = result.get(result.size() - 1);
-            in.start = Math.min(in.start, intervals.get(i).start);
-            in.end = Math.max(in.end, intervals.get(i).end);
+        while (i < intervals.size() && newInterval.end >= intervals.get(i).start) {
+            Interval last = result.get(result.size() - 1);
+            last.start = Math.min(last.start, intervals.get(i).start);
+            last.end = Math.max(last.end, intervals.get(i).end);
             i++;
         }
 
