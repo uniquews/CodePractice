7001835b88071925c3f5b0868c16b5d0c53d2acb
uniquews <wstjpu@gmail.com>
2017-09-05 21:41:19 -0400

    .

--- src/main/java/KthLargestElement.java
+++ src/main/java/KthLargestElement.java
@@ -2,38 +2,37 @@
  * Created by shuaiwang on 11/27/16.
  */
 public class KthLargestElement {
-    public int kthLargestElement(int k, int[] nums) {
-        // write your code here
+    public int findKthLargest(int[] nums, int k) {
         return helper(nums, 0, nums.length - 1, k);
     }
 
     private int helper(int[] nums, int start, int end, int k) {
-        int position = partitionn(nums, start, end);
-        if (position + 1 == k) {
-            return nums[position];
-        } else if (position + 1 < k) {
-            return helper(nums, position + 1, end, k);
+        int pos = partition(nums, start, end);
+        if (pos + 1 == k) {
+            return nums[pos];
+        } else if (pos + 1 < k) {
+            return helper(nums, pos + 1, end, k);
         } else {
-            return helper(nums, start, position - 1, k);
+            return helper(nums, start, pos - 1, k);
         }
     }
 
-    private int partitionn(int[] nums, int start, int end) {
-        int leftTail = start - 1;
+    private int partition(int[] nums, int start, int end) {
         int pivot = nums[end];
-        for (int rightHead = start; rightHead <= end - 1; rightHead++) {
-            if (nums[rightHead] >= pivot) {
-                swap(nums, leftTail + 1, rightHead);
+        int leftTail = 0;
+        for (int i = 0; i < end; i++) {
+            if (nums[i] >= pivot) {
+                swap(nums, leftTail, i);
                 leftTail++;
             }
         }
-        swap(nums, leftTail + 1, end);
-        return leftTail + 1;
+        swap(nums, leftTail, end);
+        return leftTail;
     }
 
-    private void swap(int[] nums, int start, int end) {
-        int tmp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = tmp;
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
