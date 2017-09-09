dcf1e0fb2278a1a1da0fbb167628358f8d3021bd
uniquews <wstjpu@gmail.com>
2017-09-08 22:12:47 -0400

    .

--- src/main/java/DecodeWays.java
+++ src/main/java/DecodeWays.java
@@ -3,27 +3,24 @@
  */
 public class DecodeWays {
     public int numDecodings(String s) {
-        // Write your code here
         if (s == null || s.length() == 0) {
             return 0;
         }
 
         int[] f = new int[s.length() + 1];
-
         f[0] = 1;
-        f[1] = 1;
-
-        for (int i = 2; i <= s.length(); i++) {
-            if ((s.charAt(i - 1) - '0') != 0) {
-                f[i] += f[i - 1];
+        for (int i = 1; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] = f[i - 1];
             }
+            if (i < 2)
+                continue;
+            int lastTwoDigits = Integer.valueOf(s.substring(i - 2, i));
 
-            int tmp = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
-            if (tmp <= 26 && tmp >= 10) {
+            if (lastTwoDigits >= 10 && lastTwoDigits <= 26) {
                 f[i] += f[i - 2];
             }
         }
-
         return f[s.length()];
     }
 }
