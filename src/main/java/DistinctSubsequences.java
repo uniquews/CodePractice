44b8ee13bcc772131e0804446f7f6c8f5aedbc26
uniquews <wstjpu@gmail.com>
2017-09-13 21:29:44 -0400

    .

--- src/main/java/DistinctSubsequences.java
+++ src/main/java/DistinctSubsequences.java
@@ -4,26 +4,20 @@
 public class DistinctSubsequences {
     public int numDistinct(String S, String T) {
         // write your code here
-        if (S == null || T == null) {
-            return 0;
-        }
-
-        int[][] f = new int[T.length() + 1][S.length() + 1];
+        int[][] f = new int[S.length() + 1][T.length() + 1];
 
         for (int i = 0; i <= S.length(); i++) {
-            f[0][i] = 1;
+            f[i][0] = 1;
         }
 
-        for (int i = 1; i <= T.length(); i++) {
-            for (int j = 1; j <= S.length(); j++) {
-                if (S.charAt(j - 1) == T.charAt(i - 1)) {
-                    f[i][j] = f[i][j - 1] + f[i - 1][j - 1];
-                } else {
-                    f[i][j] = f[i][j - 1];
+        for (int i = 1; i <= S.length(); i++) {
+            for (int j = 1; j <= T.length(); j++) {
+                f[i][j] = f[i - 1][j];
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    f[i][j] += f[i - 1][j - 1];
                 }
             }
         }
-
-        return f[T.length()][S.length()];
+        return f[S.length()][T.length()];
     }
 }
