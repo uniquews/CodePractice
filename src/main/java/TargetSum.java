276e36a353ee1c36bf36dea0b47ef3493788abea
uniquews <wstjpu@gmail.com>
2017-09-21 00:12:43 -0400

    .

--- src/main/java/TargetSum.java
+++ src/main/java/TargetSum.java
@@ -4,24 +4,47 @@
 public class TargetSum {
 
     // Way 1: dfs:
-    private int result = 0;
+//    private int result = 0;
+//    public int findTargetSumWays(int[] nums, int S) {
+//        dfs(nums, S, 0, 0);
+//        return result;
+//    }
+//
+//    private void dfs(int[] nums, int S, int index, int sum) {
+//        if (index == nums.length && sum == S) {
+//            result++;
+//            return;
+//        }
+//
+//        if (index == nums.length) { // 不是 if (index == nums.length || S == current) 因为index没有到底 即使S == current 后面有可能是sum = 0， 也要算上
+//            return;
+//        }
+//
+//        dfs(nums, S, index + 1, sum + nums[index]);
+//        dfs(nums, S, index + 1, sum - nums[index]);
+//    }
+
+    private int count = 0;
+    private int[] preSum;
     public int findTargetSumWays(int[] nums, int S) {
+        preSum = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
         dfs(nums, S, 0, 0);
-        return result;
+        return count;
     }
 
-    private void dfs(int[] nums, int S, int index, int sum) {
-        if (index == nums.length && sum == S) {
-            result++;
-            return;
+    private void dfs(int[] nums, int S, int index, int cur) {
+        int minusAfter = preSum[nums.length] - preSum[index];
+        if (cur - minusAfter == S) {
+            count++;
         }
 
-        if (index == nums.length) { // 不是 if (index == nums.length || S == current) 因为index没有到底 即使S == current 后面有可能是sum = 0， 也要算上
-            return;
+        for (int i = index; i < nums.length; i++) {
+            int minus = preSum[i] - preSum[index];
+            dfs(nums, S, i + 1, cur - minus + nums[i]);
         }
-
-        dfs(nums, S, index + 1, sum + nums[index]);
-        dfs(nums, S, index + 1, sum - nums[index]);
     }
 
 
