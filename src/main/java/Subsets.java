fe32d90eac4eb9cde0ccecaa07d751f494118543
uniquews <wstjpu@gmail.com>
2017-09-20 23:37:49 -0400

    .

--- src/main/java/Subsets.java
+++ src/main/java/Subsets.java
@@ -2,20 +2,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subsets {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> eachResult = new ArrayList<>();
-        helper(result, eachResult, nums, 0);
-        return result;
+//    public List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        List<Integer> eachResult = new ArrayList<>();
+//        helper(result, eachResult, nums, 0);
+//        return result;
+//    }
+//
+//    private void helper(List<List<Integer>> result, List<Integer> eachResult, int[] nums, int index) {
+//        result.add(new ArrayList<>(eachResult));
+//
+//        for (int i = index; i < nums.length; i++) {
+//            eachResult.add(nums[i]);
+//            helper(result, eachResult, nums, i + 1);
+//            eachResult.remove(eachResult.size() - 1);
+//        }
+//    }
+
+    public List<List<Integer>> subsets(int[] array) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(array, 0, res, new ArrayList<>());
+        return res;
     }
 
-    private void helper(List<List<Integer>> result, List<Integer> eachResult, int[] nums, int index) {
-        result.add(new ArrayList<>(eachResult));
-
-        for (int i = index; i < nums.length; i++) {
-            eachResult.add(nums[i]);
-            helper(result, eachResult, nums, i + 1);
-            eachResult.remove(eachResult.size() - 1);
+    private void dfs(int[] array, int i, List<List<Integer>> res, List<Integer> tmp) {
+        if (i == array.length) {
+            res.add(new ArrayList<>(tmp));
         }
+        dfs(array, i + 1, res, tmp);
+        tmp.add(array[i]);
+        dfs(array, i + 1, res, tmp);
+        tmp.remove(tmp.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3};
+        Subsets test = new Subsets();
+        test.subsets(a);
     }
 }
