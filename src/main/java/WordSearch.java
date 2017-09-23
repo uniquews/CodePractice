d4fc029b9ced89450fc100aab7b06dd1f4a84e1c
uniquews <wstjpu@gmail.com>
2017-09-22 20:36:27 -0400

    .

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -1,40 +1,50 @@
 /**
  * Created by shuaiwang on 12/2/16.
  */
+
+
+/**
+ *
+ * 这道题思路很简单，但是一定要注意出口条件的先后顺序 否则会越界
+ * */
 public class WordSearch {
+    int[] dx = {0, 0, -1, 1};
+    int[] dy = {1, -1, 0, 0};
     public boolean exist(char[][] board, String word) {
-        // write your code here
-        if (word == null || word.length() == 0) {
+        if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
             return false;
         }
         boolean[][] visited = new boolean[board.length][board[0].length];
-
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (helper(board, word, visited, i, j, 0))
-                    return true;
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, 0, i, j, visited)) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
     }
 
-    private boolean helper(char[][] board, String word, boolean[][] visited, int row, int column, int index) {
-        if (row < 0 || column < 0 || row == board.length || column == board[0].length || index == word.length() || visited[row][column] == true)
-            return false;
-
-        if (board[row][column] == word.charAt(index) && index == word.length() - 1 && visited[row][column] == false)
+    private boolean dfs(char[][] board, String word, int index, int x, int y, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
-
-        if (board[row][column] == word.charAt(index)) {
-            visited[row][column] = true;
-            boolean found = helper(board, word, visited, row + 1, column, index + 1) ||
-                    helper(board, word, visited, row, column + 1, index + 1) ||
-                    helper(board, word, visited, row - 1, column, index + 1) ||
-                    helper(board, word, visited, row, column - 1, index + 1);
-            visited[row][column] = false;
-            return found;
+        }
+        if (x < 0 || y < 0 || x == board.length || y == board[0].length || visited[x][y] ||
+                board[x][y] != word.charAt(index)) {
+            return false;
         }
 
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int m = x + dx[i];
+            int n = y + dy[i];
+            if (dfs(board, word, index + 1, m, n, visited)) {
+                return true;
+            }
+        }
+        visited[x][y] = false;
         return false;
     }
 
