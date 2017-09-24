fafeb6c63fa270d01f8d192953f881dfffc4b37b
uniquews <wstjpu@gmail.com>
2017-09-24 00:53:38 -0400

    .

--- src/main/java/RandomizedSet.java
+++ src/main/java/RandomizedSet.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by shuaiwang on 3/17/17.
@@ -11,14 +8,16 @@ import java.util.Random;
 // remove必须要和数组末尾swap 否则删除就不是O(1)
 // 不理解这种getRandom 如何保证Each element must have the same probability of being returned.
 
-public class RandomizedSet {
+class RandomizedSet {
 
+    /** Initialize your data structure here. */
+    Map<Integer, Integer> map;
     List<Integer> list;
-    HashMap<Integer, Integer> map;
-    int count;
+    Random rand;
     public RandomizedSet() {
-        list = new ArrayList<>();
         map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
@@ -27,9 +26,8 @@ public class RandomizedSet {
             return false;
         }
 
-        int pos = list.size();
+        map.put(val, list.size());
         list.add(val);
-        map.put(val, pos);
         return true;
     }
 
@@ -39,20 +37,31 @@ public class RandomizedSet {
             return false;
         }
 
-        int pos = map.get(val);
-        int tmp = list.get(list.size() - 1);
-        list.set(list.size() - 1, val);
-        list.set(pos, tmp);
-        map.put(tmp, pos);
+        int index = map.get(val);
+
+        int tmp = list.get(index);
+        list.set(index, list.get(list.size() - 1));
+        list.set(list.size() - 1, tmp);
+
+        map.put(list.get(index), index); // swap之后要记得update 被换过来的value的 index  而且要在remove之前，如果在remove之后，list 为空就越界了
         map.remove(val);
         list.remove(list.size() - 1);
+
         return true;
     }
 
     /** Get a random element from the set. */
     public int getRandom() {
-        Random rand = new Random();
-        return list.get(rand.nextInt(list.size()));
+        int index = rand.nextInt(list.size());
+        return list.get(index);
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
 
