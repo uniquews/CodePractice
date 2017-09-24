b3e0ef6d9a8c3d928b93f527894e770acaebaf5d
uniquews <wstjpu@gmail.com>
2017-09-24 11:35:57 -0400

    .

--- src/main/java/MaximalSquare.java
+++ src/main/java/MaximalSquare.java
@@ -6,28 +6,23 @@ public class MaximalSquare {
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
             return 0;
         }
+
         int[][] f = new int[matrix.length][matrix[0].length];
         int result = 0;
 
-        for (int i = 0; i < matrix[0].length; i++) {
-            f[0][i] = matrix[0][i] - '0';
-            if (matrix[0][i] == '1')
-                result = 1;
-        }
-
-        for (int i = 0; i < matrix.length; i++) {
-            f[i][0] = matrix[i][0] - '0';
-            if (matrix[i][0] == '1')
-                result = 1;
-        }
-
-
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
-                if (matrix[i][j] == '1') {
-                    f[i][j] = Math.min(f[i - 1][j - 1], Math.min(f[i - 1][j], f[i][j - 1])) + 1;
+        for (int i = 0; i < f.length; i++) {
+            for (int j = 0; j < f[i].length; j++) {
+                if (i == 0 || j == 0) {
+                    f[i][j] = matrix[i][j] - '0';
                     result = Math.max(result, f[i][j]);
+                    continue;
                 }
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+
+                f[i][j] = Math.min(f[i - 1][j - 1], Math.min(f[i - 1][j], f[i][j - 1])) + 1;
+                result = Math.max(result, f[i][j]);
             }
         }
         return result * result;
