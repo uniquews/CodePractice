2a5e768b20343f0129866611a3cfc945d4356b94
uniquews <wstjpu@gmail.com>
2017-09-27 20:35:33 -0400

    .

--- src/main/java/MaximumSubarrayII.java
+++ src/main/java/MaximumSubarrayII.java
@@ -1,36 +1,38 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by shuaiwang on 10/16/16.
  */
 public class MaximumSubarrayII {
-    public int maxTwoSubArrays(ArrayList<Integer> nums) {
-        // write your code
-        if (nums == null || nums.size() == 0) {
-            return 0;
-        }
-
+    public int maxTwoSubArrays(List<Integer> nums) {
+        // write your code here
         int[] leftMax = new int[nums.size()];
         int[] rightMax = new int[nums.size()];
 
-        int max = Integer.MIN_VALUE;
         int sum = 0;
-        int minSum = 0;
+        int maxVal = Integer.MIN_VALUE;
         for (int i = 0; i < nums.size(); i++) {
-            sum += nums.get(i);
-            max = Math.max(max, sum - minSum);
-            minSum = Math.min(sum, minSum);
-            leftMax[i] = max;
+            int currentSum = nums.get(i);
+            if (sum >= 0) {
+                currentSum += sum;
+            }
+            sum = currentSum;
+            leftMax[i] = Math.max(maxVal, currentSum);
+            maxVal = Math.max(maxVal, currentSum);
         }
 
-        max = Integer.MIN_VALUE;
         sum = 0;
-        minSum = 0;
+        maxVal = Integer.MIN_VALUE;
+
         for (int i = nums.size() - 1; i >= 0; i--) {
-            sum += nums.get(i);
-            max = Math.max(max, sum - minSum);
-            minSum = Math.min(minSum, sum);
-            rightMax[i] = max;
+            int currentSum = nums.get(i);
+            if (sum >= 0) {
+                currentSum += sum;
+            }
+            sum = currentSum;
+            rightMax[i] = Math.max(maxVal, currentSum);
+            maxVal = Math.max(maxVal, currentSum);
         }
 
         int result = Integer.MIN_VALUE;
