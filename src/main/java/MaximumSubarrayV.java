16882eefa3351b96cbcad2a1cd6177ce3126e22f
uniquews <wstjpu@gmail.com>
2017-09-27 22:30:51 -0400

    .

--- src/main/java/MaximumSubarrayV.java
+++ src/main/java/MaximumSubarrayV.java
@@ -6,6 +6,23 @@ import java.util.Queue;
  * Created by shuaiwang on 6/5/17.
  */
 public class MaximumSubarrayV {
+    // 会TLE
+//    public int maxSubarray5(int[] nums, int k1, int k2) {
+//        // write your code here
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//
+//        int[] sum = new int[nums.length + 1];
+//        int result = Integer.MIN_VALUE;
+//        for (int i = 1; i <= nums.length; i++) {
+//            sum[i] = sum[i - 1] + nums[i - 1];
+//            for (int j = i - k1; j >= 0 && j >= i - k2; j--) {
+//                result = Math.max(result, sum[i] - sum[j]);
+//            }
+//        }
+//        return result;
+//    }
 
     // 维护一个sliding window中的minSum   维护一个递减序列
     public int maxSubarray5(int[] nums, int k1, int k2) {
