ec3b6b15b0ca1ed2916e24a91615ea756a4f315a
uniquews <wstjpu@gmail.com>
2017-09-27 20:58:41 -0400

    .

--- src/main/java/MaximumSubarrayIV.java
+++ src/main/java/MaximumSubarrayIV.java
@@ -3,18 +3,21 @@
  */
 public class MaximumSubarrayIV {
     public int maxSubarray4(int[] nums, int k) {
-        // Write your code here
-        int[] sum = new int[nums.length + 1];
-        for (int i = 1; i <= nums.length; i++) {
-            sum[i] = sum[i - 1] + nums[i - 1];
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
 
+        int[] sum = new int[nums.length + 1];
+        int minVal = 0;
         int result = Integer.MIN_VALUE;
-        int minSum = 0;
-        for (int i = k; i <= nums.length; i++) {
-            result = Math.max(result, sum[i] - minSum);
-            minSum = Math.min(minSum, i - k + 1);
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+            if (i >= k) {
+                result = Math.max(result, sum[i] - minVal);
+                minVal = Math.min(minVal, sum[i - k + 1]);
+            }
         }
-        return result;
+        return result == Integer.MIN_VALUE ? 0 : result;
     }
 }
