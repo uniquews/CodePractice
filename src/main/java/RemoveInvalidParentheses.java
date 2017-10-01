c0ab6ad04a1f0970a312ed8d28586913aac06a37
uniquews <wstjpu@gmail.com>
2017-10-01 14:25:02 -0400

    .

--- src/main/java/RemoveInvalidParentheses.java
+++ src/main/java/RemoveInvalidParentheses.java
@@ -55,112 +55,66 @@ public class RemoveInvalidParentheses {
 //        return count;
 //    }
 
-
-    /**
-     *
-     * j == last_j || s.charAt(j - 1) != par[1] 表示哪一位该被删除
-     * 因为上一次删除之后j + 1位变成j last_j 记录了这个j  所以当j  (其实是上一次的j + 1) = last_j 时要删除
-     * 然而在同一层中，当j位是) 但是j - 1位也是）时，则不删，因为这样会generate 重复的dfs搜索
-     *
-     * index i之前的部分都是valid的，因为我们碰到不valid的就dfs了
-     *
-     * int j = last_j 而不是等于0  因为等于0也会生成重复：
-     *
-     * 比如将 ((k() reverse之后变成了
-     *
-     * 假设我们让j = 0
-     * )(k((
-     * 递归过程是：
-     * i = 3，j = 1， 删除j位的( => )k((
-     * dfs i从3开始，又不valid了，新的dfs j = 0，移动到 j = 2时删除( => )k(
-     * dfs之后发现valid 将(k)加入结果集，退出 回到 )k((
-     * j 继续增加到 j = 3 因为 j = 1 是第一个满足s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]), 所以避免了删除最后一个( 回到 => )(k((
-     * 此时i = 3， j = 1 j增加到3可以删除 => )(k(
-     * 由于j = 0 所以j增加到1的时候， 把 (删除了，=> )k(
-     * dfs之后发现valid讲(k)又加入结果集，因此有重复
-     *
-     *
-     * 如果我们每一次让j = last_j
-     * i = 3，j = 1， 删除j位的( => )k((
-     * j从1开始 （因为前面的都valid）j = 2时删除( => )k(
-     * dfs加入结果集(k)
-     *
-     * s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]) 跳过最后一个( 回到 )k((
-     * j 走到末尾，直接return 回 )(k((
-     * 此时i = 3， j = 1 j增加到3可以删除 => )(k(
-     * i = 3, j = 3 删除末尾的(  得到)(k
-     *
-     *
-     *
-     * */
-//    public List<String> removeInvalidParentheses(String s) {
-//        List<String> ans = new ArrayList<>();
-//        remove(s, ans, 0, 0, new char[]{'(', ')'});
-//        return ans;
-//    }
-//
-//    public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
-//        for (int stack = 0, i = last_i; i < s.length(); ++i) {
-//            if (s.charAt(i) == par[0]) stack++;
-//            if (s.charAt(i) == par[1]) stack--;
-//            if (stack >= 0) continue;
-//            for (int j = last_j; j <= i; ++j)
-//                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
-//                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
-//            return;
-//        }
-//        String reversed = new StringBuilder(s).reverse().toString();
-//        if (par[0] == '(') // finished left to right  // 如果此时已经是() 也不会被add， 要等reverse被遍历后才会被add
-//            remove(reversed, ans, 0, 0, new char[]{')', '('});
-//        else // finished right to left
-//            ans.add(reversed);
-//    }
-
     public List<String> removeInvalidParentheses(String s) {
-        int rmL = 0, rmR = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                rmL++;
-            } else if (s.charAt(i) == ')') {
-                if (rmL != 0) {
-                    rmL--;
-                } else {
-                    rmR++;
-                }
-            }
-        }
-        Set<String> res = new HashSet<>();
-        dfs(s, 0, res, new StringBuilder(), rmL, rmR, 0);
-        return new ArrayList<String>(res);
+        int[] removed = preprocess(s);
+        Set<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        helper(s, set, 0, sb, removed[0], removed[1], 0);
+        List<String> result = new ArrayList<>();
+        result.addAll(set);
+        return result;
     }
 
-    public void dfs(String s, int i, Set<String> res, StringBuilder sb, int rmL, int rmR, int open) {
-        if (rmL < 0 || rmR < 0 || open < 0) {
+    private void helper(String s, Set<String> result, int index, StringBuilder sb, int left, int right, int open) {
+        if (left < 0 || right < 0 || open < 0) {
             return;
         }
-        if (i == s.length()) {
-            if (rmL == 0 && rmR == 0 && open == 0) {
-                res.add(sb.toString());
+
+        if (index == s.length()) {
+            if (left == 0 && right == 0 && open == 0) {
+                result.add(sb.toString());
             }
             return;
         }
-
-        char c = s.charAt(i);
         int len = sb.length();
-
-        if (c == '(') {
-            dfs(s, i + 1, res, sb, rmL - 1, rmR, open);		    // not use (
-            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open + 1);       // use (
-
-        } else if (c == ')') {
-            dfs(s, i + 1, res, sb, rmL, rmR - 1, open);	            // not use  )
-            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open - 1);  	    // use )
-
+        if (s.charAt(index) == '(') {
+            helper(s, result, index + 1, sb, left - 1, right, open);
+            sb.append('(');
+            helper(s, result, index + 1, sb, left, right, open + 1);
+        } else if (s.charAt(index) == ')') {
+            helper(s, result, index + 1, sb, left, right - 1, open);
+            sb.append(')');
+            helper(s, result, index + 1 , sb, left, right, open - 1);
         } else {
-            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open);
+            sb.append(s.charAt(index));
+            helper(s, result, index + 1, sb, left, right, open);
         }
-
         sb.setLength(len);
+    }
+
+    private int[] preprocess(String s) {
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '(' && s.charAt(i) != ')') {
+                continue;
+            }
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                left--;
+            }
+
+            if (left < 0) {
+                right++;
+                left = 0;
+            }
+        }
+        int[] result = new int[2];
+        result[0] = left;
+        result[1] = right;
+        return result;
     }
 
     public static void main(String[] args) {
