80e47afd71c697849697de4dfb3a8d05e2c31fd5
uniquews <wstjpu@gmail.com>
2017-10-03 18:34:13 -0400

    .

--- src/main/java/WordDictionary.java
+++ src/main/java/WordDictionary.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by shuaiwang on 12/3/16.
  */
@@ -69,82 +72,159 @@ public class WordDictionary {
 //        return trieTree.find(word, trieTree.root, 0);
 //    }
 
+//    class TrieNode {
+//        public TrieNode[] children = null;
+//        public boolean hasWord = false;
+//        public TrieNode() {
+//            children = new TrieNode[26];
+//        }
+//    }
+//
+//    class Trie {
+//        public TrieNode root;
+//        public Trie() {
+//            root = new TrieNode();
+//        }
+//
+//        public void add(String word) {
+//            TrieNode current = root;
+//            for (int i = 0; i < word.length(); i++) {
+//                if (current.children[word.charAt(i) - 'a'] == null) {
+//                    TrieNode next = new TrieNode();
+//                    current.children[word.charAt(i) - 'a'] = next;
+//                }
+//                current = current.children[word.charAt(i) - 'a'];
+//                if (i == word.length() - 1) {
+//                    current.hasWord = true;
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    Trie trie = new Trie();
+//    // Adds a word into the data structure.
+//    public void addWord(String word) {
+//        // Write your code here
+//        trie.add(word);
+//    }
+//
+//    // Returns if the word is in the data structure. A word could
+//    // contain the dot character '.' to represent any one letter.
+//    public boolean search(String word) {
+//        // Write your code here
+//        TrieNode current = trie.root;
+//        return search(word, current, 0);
+//    }
+//
+//    private boolean search(String word, TrieNode current, int index) {
+//        if (index == word.length() && current.hasWord)
+//            return true;
+//
+//        if (index == word.length())
+//            return false;
+//
+//        boolean result = false;
+//        if (word.charAt(index) == '.') {
+//            for (int i = 0; i < 26; i++) {
+//                if (current.children[i] != null) {
+//                    result = result || search(word, current.children[i], index + 1);
+//                }
+//            }
+//        } else {
+//            if (current.children[word.charAt(index) - 'a'] == null)
+//                return false;
+//            result = search(word, current.children[word.charAt(index) - 'a'], index + 1);
+//        }
+//        return result;
+//    }
+
     class TrieNode {
-        public TrieNode[] children = null;
-        public boolean hasWord = false;
+        Map<Character, TrieNode> children;
+        boolean hasWord;
+
         public TrieNode() {
-            children = new TrieNode[26];
+            children = new HashMap<>();
+            hasWord = false;
         }
     }
 
     class Trie {
-        public TrieNode root;
+        TrieNode root;
         public Trie() {
             root = new TrieNode();
         }
 
-        public void add(String word) {
-            TrieNode current = root;
-            for (int i = 0; i < word.length(); i++) {
-                if (current.children[word.charAt(i) - 'a'] == null) {
-                    TrieNode next = new TrieNode();
-                    current.children[word.charAt(i) - 'a'] = next;
-                }
-                current = current.children[word.charAt(i) - 'a'];
-                if (i == word.length() - 1) {
-                    current.hasWord = true;
-                }
+        public void add(String s) {
+            TrieNode cur = root;
+            for (int i = 0; i < s.length(); i++) {
+                Map<Character, TrieNode> children = cur.children;
+                TrieNode next = children.getOrDefault(s.charAt(i), new TrieNode());
+                cur.children.put(s.charAt(i), next);
+                cur = next;
             }
+            cur.hasWord = true;
         }
 
+        public boolean search(String s, int index, TrieNode cur) {
+            if (index == s.length()) {
+                return cur.hasWord;
+            }
+
+            boolean result = false;
+            char c = s.charAt(index);
+            if (c != '.') {
+                if (!cur.children.containsKey(c)) {
+                    return false;
+                }
+                result = search(s, index + 1, cur.children.get(c));
+            } else {
+                for (Character tmp : cur.children.keySet()) {
+                    if (search(s, index + 1, cur.children.get(tmp))) {
+                        return true;
+                    }
+                }
+            }
+            return result;
+        }
+
+
+    }
+    /** Initialize your data structure here. */
+    Trie trie;
+    public WordDictionary() {
+        trie = new Trie();
     }
 
-    Trie trie = new Trie();
-    // Adds a word into the data structure.
+    /** Adds a word into the data structure. */
     public void addWord(String word) {
-        // Write your code here
         trie.add(word);
     }
 
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     public boolean search(String word) {
-        // Write your code here
-        TrieNode current = trie.root;
-        return search(word, current, 0);
-    }
-
-    private boolean search(String word, TrieNode current, int index) {
-        if (index == word.length() && current.hasWord)
-            return true;
-
-        if (index == word.length())
-            return false;
-
-        boolean result = false;
-        if (word.charAt(index) == '.') {
-            for (int i = 0; i < 26; i++) {
-                if (current.children[i] != null) {
-                    result = result || search(word, current.children[i], index + 1);
-                }
-            }
-        } else {
-            if (current.children[word.charAt(index) - 'a'] == null)
-                return false;
-            result = search(word, current.children[word.charAt(index) - 'a'], index + 1);
-        }
-        return result;
+        return trie.search(word, 0, trie.root);
     }
 
     public static void main(String[] args) {
         WordDictionary wd = new WordDictionary();
         // search("lintcode"), search("lint"), addWord("lint"), search("lint")
 
-        wd.addWord("a");
-        wd.search(".");
-        System.out.print(wd.search("lintcode"));
-        System.out.print(wd.search("lint"));
-        wd.addWord("lint");
-        System.out.print(wd.search("lint"));
+        /**
+         * ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+         [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+         *
+         * */
+        wd.addWord("bad");
+        wd.addWord("dad");
+        wd.addWord("mad");
+        wd.addWord("pad");
+        wd.addWord("bad");
+        wd.search(".ad");
+        wd.search("b..");
+//        System.out.print(wd.search("lintcode"));
+//        System.out.print(wd.search("lint"));
+//        wd.addWord("lint");
+//        System.out.print(wd.search("lint"));
     }
 }
