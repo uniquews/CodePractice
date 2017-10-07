aa34d7ddff273beb7cb81ee103f42fb7414ebc38
uniquews <wstjpu@gmail.com>
2017-10-06 22:39:56 -0400

    .

--- src/main/java/BSTIterator.java
+++ src/main/java/BSTIterator.java
@@ -6,67 +6,33 @@ import java.util.Stack;
  * Created by shuaiwang on 8/16/16.
  */
 public class BSTIterator {
-//
-//    Stack<TreeNode> stk = new Stack<>();
-//    TreeNode current;
-//
-//    //@param root: The root of binary tree.
-//    public BSTIterator(TreeNode root) {
-//        // write your code here
-//        current = root;
-//    }
-//
-//    //@return: True if there has next node, or false
-//    public boolean hasNext() {
-//        // write your code here
-//        return current != null || !stk.isEmpty();
-//    }
-//
-//    //@return: return next node
-//    public TreeNode next() {
-//        // write your code here
-//        while (current != null) {
-//            stk.push(current);
-//            current = current.left;
-//        }
-//
-//        current = stk.pop();
-//        TreeNode node = current;
-//        current = current.right;
-//        return node;
-//    }
-
     Stack<TreeNode> stk;
-    TreeNode current;
+    TreeNode cur;
     public BSTIterator(TreeNode root) {
         stk = new Stack<>();
-        current = root;
+        cur = root;
     }
 
-    private TreeNode helper() {
-        TreeNode result = null;
-        while (!stk.isEmpty() || current != null) {
-            if (current != null) {
-                stk.push(current);
-                current = current.left;
-            } else {
-                current = stk.pop();
-                result = current;
-                current = current.right;
-                break;
-            }
+    private Integer helper() {
+        while (cur != null) {
+            stk.push(cur);
+            cur = cur.left;
         }
+
+        cur = stk.pop();
+        Integer result = cur.val;
+        cur = cur.right;
         return result;
     }
 
     /** @return whether we have a next smallest number */
     public boolean hasNext() {
-        return !stk.isEmpty() || current != null;
+        return cur != null || !stk.isEmpty();
     }
 
     /** @return the next smallest number */
     public int next() {
-        TreeNode result = helper();
-        return result.val;
+        return helper();
     }
 }
+
