069914651d60b74a889b19e90184f8d807030972
uniquews <wstjpu@gmail.com>
2017-10-07 16:41:18 -0400

    .

--- src/main/java/LongestArithmeticSubsequence.java
+++ src/main/java/LongestArithmeticSubsequence.java
@@ -51,35 +51,100 @@ public class LongestArithmeticSubsequence {
      * f[i][j] = f[k][i] + 1 nums[j] - nums[i] == nums[i] - nums[k]
      * 可以不排序
      * */
+//    public int lenghtOfLongestAP(int[] nums) {
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//
+//        int count = 0;
+//        int[][] f = new int[nums.length][nums.length];
+//        for (int i = 0; i < f.length - 1; i++) {
+//            for (int j = i + 1; j < f.length; j++) {
+//                f[i][j] = 2;
+//            }
+//        }
+//
+//        for (int j = 2; j < nums.length; j++) {
+//            for (int i = j - 1; i >= 1; i--) {
+//                for (int k = i - 1; k >=0; k--) {
+//                    if (nums[j] - nums[i] == nums[i] - nums[k]) {
+//                        f[i][j] = Math.max(f[i][j], f[k][i] + 1);
+//                    }
+//                }
+//                count = Math.max(count, f[i][j]);
+//            }
+//        }
+//        return count;
+//    }
+
+//    public int lenghtOfLongestAP(int[] nums) {
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//
+//        int[][] f = new int[nums.length][nums.length];
+//        for (int i = 1; i < nums.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                f[i][j] = 2;
+//            }
+//        }
+//
+//        int result = 2;
+//        for (int i = 2; i < nums.length; i++) {
+//            for (int j = 1; j < i; j++) {
+//                for (int k = 0; k < j; k++) {
+//                    if (nums[i] - nums[j] == nums[j] - nums[k]) {
+//                        f[i][j] = Math.max(f[i][j], f[j][k] + 1);
+//                    }
+//                }
+//                result = Math.max(result, f[i][j]);
+//            }
+//        }
+//        return result;
+//
+//    }
     public int lenghtOfLongestAP(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        int count = 0;
         int[][] f = new int[nums.length][nums.length];
-        for (int i = 0; i < f.length - 1; i++) {
-            for (int j = i + 1; j < f.length; j++) {
-                f[i][j] = 2;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                f[j][i] = 2;
             }
         }
 
-        for (int j = 2; j < nums.length; j++) {
-            for (int i = j - 1; i >= 1; i--) {
-                for (int k = i - 1; k >=0; k--) {
-                    if (nums[j] - nums[i] == nums[i] - nums[k]) {
-                        f[i][j] = Math.max(f[i][j], f[k][i] + 1);
-                    }
+        int result = 2;
+        for (int j = 1; j < nums.length - 1; j++) {
+            int i = j - 1;
+            int k = j + 1;
+
+            while (i >= 0 && k < nums.length) {
+                int tmp1 = nums[j] - nums[i];
+                int tmp2 = nums[k] - nums[j];
+
+                if (tmp1 == tmp2) {
+                    f[j][k] = Math.max(f[j][k], f[i][j] + 1);
+                    result = Math.max(result, f[j][k]);
+                    i--;
+                    k++;
+                } else if (tmp1 < tmp2) {
+                    i--;
+                } else {
+                    k++;
                 }
-                count = Math.max(count, f[i][j]);
             }
         }
-        return count;
+        return result;
     }
 
     public static void main(String[] args) {
         LongestArithmeticSubsequence test = new LongestArithmeticSubsequence();
-        int[] nums = {2, 4, 6, 8, 10};
+//        int[] nums = {2, 4, 6, 8, 10};
+//        int[] nums = {1, 7, 10, 13, 14, 19};
+        int[] nums = {1, 7, 10, 15, 27, 29};
+//        int[] nums = {1,3,5};
         System.out.println(test.lenghtOfLongestAP(nums));
     }
 }
