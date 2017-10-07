34cf7d51dd85bbeb3823903a19e36c81eef0f3b1
uniquews <wstjpu@gmail.com>
2017-10-07 16:53:03 -0400

    .

--- src/main/java/FindtheCelebrity.java
+++ src/main/java/FindtheCelebrity.java
@@ -3,41 +3,27 @@
  */
 public class FindtheCelebrity {
     public int findCelebrity(int n) {
-        for (int i = 0; i < n; i++) {
-            int j = 0;
-            for (; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
-
-                if (knows(i, j) || !knows(j, i)) {
-                    break;
-                }
-            }
-            if (j == n) {
-                return i;
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(candidate, i)) {
+                candidate = i;
             }
         }
-        return -1;
-    }
 
-//    public int findCelebrity(int n) {
-//        int candidate = 0;
-//        for (int i = 1; i < n; i++) {
-//            if (knows(candidate, i)) {
-//                candidate = i;
-//            }
-//        }
-//
-//        for (int i = 0; i < n; i++) {
-//            if (candidate == i)
-//                continue;
-//            if (knows(candidate, i) || !knows(i, candidate)) {
-//                return -1;
-//            }
-//        }
-//        return candidate;
-//    }
+        for (int i = 0; i < candidate; i++) {
+            if (!(knows(i, candidate) && !knows(candidate, i))) {
+                return -1;
+            }
+        }
+
+        for (int i = candidate + 1; i < n; i++) {
+            if (!knows(i, candidate)) {
+                return -1;
+            }
+        }
+
+        return candidate;
+    }
 
     boolean knows(int a, int b) {
         return true;
