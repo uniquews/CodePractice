100269824f4977922f794a7ddcd3abdfec198580
uniquews <wstjpu@gmail.com>
2017-10-14 18:20:44 -0400

    .

--- src/main/java/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/java/RemoveDuplicatesFromSortedArrayII.java
@@ -3,15 +3,27 @@
  */
 public class RemoveDuplicatesFromSortedArrayII {
     public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0)
+        if (nums == null) {
             return 0;
+        }
+
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
         int index = 2;
         for (int i = 2; i < nums.length; i++) {
-            if (nums[index - 2] == nums[i] && nums[index - 1] == nums[i]) {
+            if (nums[i] == nums[index - 2]) {
                 continue;
             }
             nums[index++] = nums[i];
         }
         return index;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArrayII test = new RemoveDuplicatesFromSortedArrayII();
+        int[] a = {1,1,1,2,2,3};
+        test.removeDuplicates(a);
     }
 }
