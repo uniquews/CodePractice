843f757146d320df9190f28e7df7ff821798b2fe
uniquews <wstjpu@gmail.com>
2017-10-15 19:43:52 -0400

    .

--- src/main/java/RegularExpressionMatching.java
+++ src/main/java/RegularExpressionMatching.java
@@ -14,50 +14,71 @@ public class RegularExpressionMatching {
 
              dp[i][j] = dp[i-1][j]  test case: s = "aaa", p = ".*"
     * */
+//    public boolean isMatch(String s, String p) {
+//        boolean[][] f = new boolean[s.length() + 1][p.length() + 1];
+//
+//        //如果s为空但是p不为空，f[0][i]还有可能是true
+//        //如果s不为空但是p为空，f[i][0]肯定是false，所以不需要初始化
+//        //为了后面引用 f[0][0]需要是true
+//
+//        f[0][0] = true;
+//        for (int i = 1; i <= p.length(); i++) {
+//            if (p.charAt(i - 1) == '*') {
+//                if (i == 1) {
+//                    f[0][i] = true;
+//                } else {
+//                    f[0][i] = f[0][i - 2];
+//                }
+//            }
+//        }
+//
+//        for (int i = 1; i <= s.length(); i++) {
+//            for (int j = 1; j <= p.length(); j++) {
+//                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+//                    f[i][j] = f[i - 1][j - 1];
+//                } else if (p.charAt(j - 1) == '*') {
+//                    f[i][j] = f[i][j - 1];
+//                    if (j >= 2) {
+//                        f[i][j] = f[i][j] || f[i][j - 2];
+//                        if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+//                            // s = ab p = d* inorder to let i-1 match j, you have to make sure
+//                            // that s.char(i - 1) == p.charAt(j-2) Otherwise in this case,
+//                            // we will let a match d*, which gets true without checking if a == b
+//                            // however ad* should match d*
+//                            f[i][j] = f[i][j] || f[i - 1][j];
+//                        }
+//                    }
+//
+//                }
+//            }
+//        }
+//        return f[s.length()][p.length()];
+//    }
+
     public boolean isMatch(String s, String p) {
-        boolean[][] f = new boolean[s.length() + 1][p.length() + 1];
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
 
-        //如果s为空但是p不为空，f[0][i]还有可能是true
-        //如果s不为空但是p为空，f[i][0]肯定是false，所以不需要初始化
-        //为了后面引用 f[0][0]需要是true
-
-        f[0][0] = true;
-        for (int i = 1; i <= p.length(); i++) {
-            if (p.charAt(i - 1) == '*') {
-                if (i == 1) {
-                    f[0][i] = true;
-                } else {
-                    f[0][i] = f[0][i - 2];
-                }
+        if (s.isEmpty()) {
+            if (p.length() < 2 || p.charAt(1) != '*') {
+                return false;
             }
         }
 
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 1; j <= p.length(); j++) {
-                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
-                    f[i][j] = f[i - 1][j - 1];
-                } else if (p.charAt(j - 1) == '*') {
-                    f[i][j] = f[i][j - 1];
-                    if (j >= 2) {
-                        f[i][j] = f[i][j] || f[i][j - 2];
-                        if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
-                            // s = ab p = d* inorder to let i-1 match j, you have to make sure
-                            // that s.char(i - 1) == p.charAt(j-2) Otherwise in this case,
-                            // we will let a match d*, which gets true without checking if a == b
-                            // however ad* should match d*
-                            f[i][j] = f[i][j] || f[i - 1][j];
-                        }
-                    }
-
-                }
-            }
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || !s.isEmpty() && isMatch(s.substring(1), p) && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
         }
-        return f[s.length()][p.length()];
+
+        if (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)) {
+            return !s.isEmpty() && isMatch(s.substring(1), p.substring(1));
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        String s = "abcd";
-        String p = "d*";
+        String s = "ab";
+        String p = ".*c";
         RegularExpressionMatching test = new RegularExpressionMatching();
         test.isMatch(s, p);
     }
