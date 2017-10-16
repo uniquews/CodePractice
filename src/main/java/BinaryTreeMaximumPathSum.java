ade34bd3f21676ea710272f3b3f83bfe6258656e
uniquews <wstjpu@gmail.com>
2017-10-16 18:38:15 -0400

    .

--- src/main/java/BinaryTreeMaximumPathSum.java
+++ src/main/java/BinaryTreeMaximumPathSum.java
@@ -4,34 +4,56 @@ import utils.TreeNode;
  * Created by shuaiwang on 7/27/16.
  */
 public class BinaryTreeMaximumPathSum {
-    private class ResultType {
-        int singlePath;
-        int maxPath;
+//    private class ResultType {
+//        int singlePath;
+//        int maxPath;
+//
+//        ResultType(int singlePath, int maxPath) {
+//            this.singlePath = singlePath;
+//            this.maxPath = maxPath;
+//        }
+//    }
+//
+//    public int maxPathSum(TreeNode root) {
+//        // write your code here
+//        return helper(root).maxPath;
+//    }
+//
+//    private ResultType helper(TreeNode n) {
+//        if (n == null) {
+//            return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
+//        }
+//
+//        ResultType leftResult = helper(n.left);
+//        ResultType rightResult = helper(n.right);
+//
+//        int singlePath = n.val + Math.max(0, Math.max(leftResult.singlePath, rightResult.singlePath));
+//
+//        int maxFromChildren = Math.max(leftResult.maxPath, rightResult.maxPath);
+//        int maxPath = Math.max(maxFromChildren, (Math.max(leftResult.singlePath, 0) + Math.max(rightResult.singlePath, 0) + n.val));
+//        return new ResultType(singlePath, maxPath);
+//    }
 
-        ResultType(int singlePath, int maxPath) {
-            this.singlePath = singlePath;
-            this.maxPath = maxPath;
-        }
-    }
-
+    int maxSum;
     public int maxPathSum(TreeNode root) {
-        // write your code here
-        return helper(root).maxPath;
+        maxSum = Integer.MIN_VALUE;
+        helper(root);
+        return maxSum;
     }
 
-    private ResultType helper(TreeNode n) {
+    private int helper(TreeNode n) {
         if (n == null) {
-            return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
+            return Integer.MIN_VALUE;
         }
 
-        ResultType leftResult = helper(n.left);
-        ResultType rightResult = helper(n.right);
+        int leftNode = helper(n.left);
+        int rightNode = helper(n.right);
 
-        int singlePath = n.val + Math.max(0, Math.max(leftResult.singlePath, rightResult.singlePath));
+        int result = Math.max(0, Math.max(leftNode, rightNode)) + n.val;
 
-        int maxFromChildren = Math.max(leftResult.maxPath, rightResult.maxPath);
-        int maxPath = Math.max(maxFromChildren, (Math.max(leftResult.singlePath, 0) + Math.max(rightResult.singlePath, 0) + n.val));
-        return new ResultType(singlePath, maxPath);
+        int tmp1 = n.val + Math.max(0, leftNode) + Math.max(0, rightNode);
+        maxSum = Math.max(maxSum, Math.max(tmp1, Math.max(leftNode, rightNode)));
+        return result;
     }
 
 }
