6eff48fa5cda097b0b187e97a8f6180af3342eb2
uniquews <wstjpu@gmail.com>
2017-10-25 21:41:41 -0400

    .

--- src/main/java/PalindromePartitioningII.java
+++ src/main/java/PalindromePartitioningII.java
@@ -1,43 +1,40 @@
+import java.util.Arrays;
+
 /**
  * Created by shuaiwang on 8/30/16.
  */
 public class PalindromePartitioningII {
+    // f[i] = f[j] + 1 if  f[j] is not Integer.MAX and s[j + 1][i] is palindrome
     public int minCut(String s) {
-        // write your code here
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
         int[] f = new int[s.length() + 1];
+        boolean[][] map = new boolean[s.length()][s.length()];
 
+        Arrays.fill(f, Integer.MAX_VALUE);
         f[0] = -1;
 
         for (int i = 1; i <= s.length(); i++) {
-            f[i] = i - 1;
-        }
-
-        for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if (isPalindrome(s.substring(j, i))) {
-                    f[i] = Math.min(f[i], f[j] + 1);
+                if (f[j] == Integer.MAX_VALUE || !isValid(map, s, j, i - 1)) {
+                    continue;
                 }
+                f[i] = Math.min(f[j] + 1, f[i]);
             }
         }
         return f[s.length()];
     }
 
-    private boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-
-        while (start <= end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-
-            start++;
-            end--;
+    private boolean isValid(boolean[][] map, String s, int i, int j) {
+        if (i == j) {
+            map[i][i] = true;
+            return true;
         }
-        return true;
+
+        if (i == j - 1) {
+            map[i][j] = s.charAt(i) == s.charAt(j);
+            return map[i][j];
+        }
+
+        map[i][j] = map[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+        return map[i][j];
     }
 }
