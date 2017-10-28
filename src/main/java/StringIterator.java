f28c8761c6a3a8d9d79af8c2532694b957c571df
uniquews <wstjpu@gmail.com>
2017-10-28 11:44:58 -0400

    .

--- src/main/java/StringIterator.java
+++ src/main/java/StringIterator.java
@@ -2,49 +2,39 @@
  * Created by shuaiwang on 7/16/17.
  */
 public class StringIterator {
-    private char c;
-    private int occur = 0;
-    private int index = 0;
-    private String s = null;
+    private char cur;
+    private int num;
+    private int index;
+    private String s;
     public StringIterator(String compressedString) {
         s = compressedString;
-        getChar();
+        helper();
+    }
+
+    private void helper() {
+        while (index < s.length() && num == 0) {
+            cur = s.charAt(index);
+            int j = index + 1;
+            while (j < s.length() && Character.isDigit(s.charAt(j))) {
+                j++;
+            }
+            num = Integer.parseInt(s.substring(index + 1, j));
+            index = j;
+        }
     }
 
     public char next() {
-        char result = c;
-        if (occur > 0)
-            occur--;
-        getChar();
-        return result;
+        helper();
+        if (num == 0) {
+            return ' ';
+        } else {
+            num--;
+            return cur;
+        }
     }
 
     public boolean hasNext() {
-        if (occur != 0)
-            return true;
-        if (index != s.length())
-            return true;
-        return false;
-    }
-
-    private void getChar() {
-        if (occur > 0)
-            return;
-        if (index == s.length()) {
-            c = ' ';
-            return;
-        }
-        c = s.charAt(index++);
-        getNum();
-    }
-
-    private void getNum() {
-        int start = index, end = index;
-        while (end < s.length() && Character.isDigit(s.charAt(end))) {
-            end++;
-        }
-        occur = Integer.valueOf(s.substring(start, end));
-        index = end;
+        return num != 0 || index != s.length();
     }
 
     public static void main(String[] args) {
