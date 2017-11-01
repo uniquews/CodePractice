280b3db8ed17b6afb08ca89163cda92045e1699c
uniquews <wstjpu@gmail.com>
2017-10-31 20:02:40 -0400

    .

--- src/main/java/DiagonalTraverse.java
+++ src/main/java/DiagonalTraverse.java
@@ -40,45 +40,26 @@ public class DiagonalTraverse {
 //    }
 
     public int[] findDiagonalOrder(int[][] matrix) {
-        int[] result = new int[matrix.length * matrix[0].length];
-        boolean up = true; // direction
+        if (matrix == null || matrix.length == 0) return new int[0];
+        int m = matrix.length, n = matrix[0].length;
 
-        int i = 0, j = 0, index = 0;
-        while (index < result.length) {
-            result[index++] = matrix[i][j];
+        int[] result = new int[m * n];
+        int row = 0, col = 0, d = 0;
+        int[][] dirs = {{-1, 1}, {1, -1}};
 
-            if (up) {
-                i -= 1;
-                j += 1;
-            } else {
-                i += 1;
-                j -= 1;
-            }
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+            row += dirs[d][0];
+            col += dirs[d][1];
 
-            if (i < 0 && j == matrix[0].length) { //右上角
-                i += 2;
-                j -= 1;
-                up = !up;
-            } else if (i == matrix.length && j < 0) { //左下角
-                j += 2;
-                i -= 1;
-                up = !up;
-            } else if (i < 0) { // 上边界
-                i += 1;
-                up = !up;
-            } else if (j == matrix[0].length) { // 右边界
-                i += 2;
-                j -= 1;
-                up = !up;
-            } else if (i == matrix.length) { // 下边界
-                j += 2;
-                i -= 1;
-                up = !up;
-            } else if (j < 0) { // 左边界
-                j += 1;
-                up = !up;
-            }
+
+            if (row >= m) { row = m - 1; col += 2; d = 1 - d;}
+            if (col >= n) { col = n - 1; row += 2; d = 1 - d;}
+            if (row < 0)  { row = 0; d = 1 - d;}  // row < 0 时 col也有可能>= n 右上角
+            if (col < 0)  { col = 0; d = 1 - d;} // col < 0 是row 也可能 >= n  左下角
+
         }
+
         return result;
     }
 
