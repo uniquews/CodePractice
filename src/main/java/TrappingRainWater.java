8d7dc2a2644469101a5971f8f16dafe5fe1c300a
uniquews <wstjpu@gmail.com>
2017-11-11 15:19:32 -0500

    .

--- src/main/java/TrappingRainWater.java
+++ src/main/java/TrappingRainWater.java
@@ -4,34 +4,32 @@
 
 //比较首尾两根柱子，矮柱子旁边的那根柱子的最终高度是可以被确定的。 移动矮柱子的指针， 问题规模变为n - 1
 public class TrappingRainWater {
-    public int trapRainWater(int[] height) {
-        // write your code here
+    public int trap(int[] height) {
         if (height == null || height.length == 0) {
             return 0;
         }
 
-        int start = 0, end = height.length - 1, water = 0;
-        while (start + 1 < end) {
-            if (height[start] <= height[end] && start + 1 < end) {
-                if (height[start] - height[start + 1] > 0) {
-                    water += height[start] - height[start + 1];
-                    height[start + 1] = height[start];
-                }
-                start++;
-            } else if (height[end] < height[start] && start + 1 < end) {
-                if (height[end] - height[end - 1] > 0) {
-                    water += height[end] - height[end - 1];
-                    height[end - 1] = height[end];
-                }
-                end--;
+        int left = 0;
+        int right = height.length - 1;
+        int result = 0;
+
+        while (left + 1 < right) {
+            if (height[left] <= height[right]) {
+                result += height[left] > height[left + 1] ? height[left] - height[left + 1] : 0;
+                height[left + 1] = height[left] > height[left + 1] ? height[left] : height[left + 1];
+                left++;
+            } else {
+                result += height[right] > height[right - 1] ? height[right] - height[right - 1] : 0;
+                height[right - 1] = height[right] > height[right - 1] ? height[right] : height[right - 1];
+                right--;
             }
         }
-        return water;
+        return result;
     }
 
     public static void main(String[] args) {
         int[] f = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         TrappingRainWater test = new TrappingRainWater();
-        System.out.print(test.trapRainWater(f));
+        System.out.print(test.trap(f));
     }
 }
