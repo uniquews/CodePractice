06900cefee395ba938fc14a9b60f248a2ad5712b
uniquews <wstjpu@gmail.com>
2017-11-12 22:34:07 -0500

    .

--- src/main/java/FindMinimuminRotatedSortedArrayII.java
+++ src/main/java/FindMinimuminRotatedSortedArrayII.java
@@ -2,34 +2,27 @@
  * Created by shuaiwang on 5/24/17.
  */
 public class FindMinimuminRotatedSortedArrayII {
-    public int findMin(int[] num) {
-        // write your code here
-        int start = 0, end = num.length - 1;
-        while (start + 1 < end) {
-            if (num[start] < num[end]) {
-                return num[start];
-            }
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
 
-            if (num[start] == num[end]) {
-                start++;
+        while (start + 1 < end) {
+            if (nums[start] < nums[end]) {
+                return nums[start];
+            }
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else if (nums[start] < nums[mid]) {
+                start = mid;
             } else {
-                int mid = start + (end - start) / 2;
-                if (num[start] < num[mid]) {
-                    start = mid;
-                } else if (num[start] == num[mid]) {
-                    start++;
-                } else if (num[mid] < num[end]) {
-                    end = mid;
-                } else if (num[mid] == num[end]) {
-                    end--;
-                }
+                start++;
             }
         }
-
-        if (num[start] <= num[end]) {
-            return num[start];
+        if (nums[start] < nums[end]) {
+            return nums[start];
         } else {
-            return num[end];
+            return nums[end];
         }
     }
 }
